0169ed63aa68d67cd6d55d870e8f67b6d06911c0
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-06-29 19:51:14 -0400

    add fs-7 font size

--- app.vue
+++ app.vue
@@ -4,22 +4,26 @@
   </NuxtLayout>
 </template>
 <style lang="scss">
-  @import url('https://fonts.cdnfonts.com/css/tellural');
+@import url('https://fonts.cdnfonts.com/css/tellural');
 
-  body {
-    font-family: 'Poppins', sans-serif;
-  }
+body {
+  font-family: 'Poppins', sans-serif;
+}
 
-  .tellural {
-    font-family: 'tellural', sans-serif;
-  }
+.tellural {
+  font-family: 'tellural', sans-serif;
+}
 
-  p {
-    margin: 0;
-  }
+p {
+  margin: 0;
+}
 
-  $green: #B5CD13;
-  $blue: #0EB3E0;
-  $red: #FF3A3A;
+$green: #B5CD13;
+$blue: #0EB3E0;
+$red: #FF3A3A;
+
+.fs-7 {
+  font-size: .85rem;
+}
 </style>
                 
