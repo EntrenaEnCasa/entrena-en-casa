6c8f199047b7fd28e68f3a8bd616d6487c8e1074
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-06-29 15:21:19 -0400

    fix header

--- components/Header.vue
+++ components/Header.vue
@@ -1,15 +1,15 @@
 <template>
-    
-    <nav class="navbar navbar-expand-lg bg-transparent">
-        <div class="container-fluid w-30 d-flex justify-content-sm-evenly">
+    <nav class="navbar navbar-expand-lg bg-gradiente">
+        <div class="container">
             <a class="navbar-brand" href="#">
                 <img src="/logo-horizontal.png" class="object-fit-contain" alt="Bootstrap" width="150" height="50">
             </a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="d-flex " id="navbarSupportedContent">
-                <ul class="navbar-nav gap-2 justify-content-center me-auto mb-2 mb-lg-0">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav gap-2 mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Inicio</a>
                     </li>
@@ -19,19 +19,25 @@
                     <li class="nav-item ">
                         <a class="nav-link " href="#">Planes</a>
                     </li>
-                </ul>   
-                
-            </div>
-            <div>
-                <ul class="navbar-nav justify-content-center me-auto mb-2 mb-lg-0">
-                    <li class="nav-item p-2">
-                        <a class="nav-link " href="#">Registro</a>
-                    </li>
-                    <li class="nav-item p-2">
-                        <button type="button" class="btn btn-outline-success">Iniciar sesión</button> 
-                    </li>
                 </ul>
+                <div class="ms-auto">
+                    <ul class="navbar-nav justify-content-center me-auto mb-2 mb-lg-0">
+                        <li class="nav-item p-2">
+                            <a class="nav-link " href="#">Registro</a>
+                        </li>
+                        <li class="nav-item p-2">
+                            <button type="button" class="btn btn-outline-success">Iniciar sesión</button>
+                        </li>
+                    </ul>
+                </div>
+
             </div>
         </div>
     </nav>
 </template>
+<style>
+.bg-gradiente {
+    background: url('/background.svg') no-repeat center center fixed;
+    /* background: linear-gradient(136deg, rgba(198, 243, 255, 0.15) 0%, rgba(113, 212, 240, 0.15) 64.54%, rgba(0, 142, 181, 0.15) 100%); */
+}
+</style>
