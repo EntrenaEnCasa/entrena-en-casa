faeee28e16819bcc0cef2a9bef65cb18985666f7
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-06-29 19:44:26 -0400

    global variables
    colors and fonts

--- app.vue
+++ app.vue
@@ -3,19 +3,23 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
-<style>
-@import url('https://fonts.cdnfonts.com/css/tellural');
+<style lang="scss">
+  @import url('https://fonts.cdnfonts.com/css/tellural');
 
-body {
-  font-family: 'Poppins', sans-serif;
-}
+  body {
+    font-family: 'Poppins', sans-serif;
+  }
 
-.tellural {
-  font-family: 'tellural', sans-serif;
-}
+  .tellural {
+    font-family: 'tellural', sans-serif;
+  }
 
-p {
-  margin: 0;
-}
+  p {
+    margin: 0;
+  }
+
+  $green: #B5CD13;
+  $blue: #0EB3E0;
+  $red: #FF3A3A;
 </style>
                 
