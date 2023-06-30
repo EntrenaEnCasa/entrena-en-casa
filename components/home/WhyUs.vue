3c2750fba0a797806ace49ca47d693d5c4831280
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-06-29 23:02:26 -0400

    add WhyUs component

---
+++ components/home/WhyUs.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="text-center" style="margin-top: 100px; margin-bottom: 20px;">
+        <p style="letter-spacing: .3rem;">BENEFICIOS</p>
+        <h2 class="fw-medium my-2 fs-1">¿Por qué Entrena en Casa?</h2>
+        <p class="fw-light">Entrena en Casa es la nueva forma de ejercitarse y vivir de forma más saludable.</p>
+        <div class="d-flex my-5 justify-between">
+            <img class="position-absolute" src="/home/woman-exercising.png" alt="woman exercising" width="500">
+            <div class="d-flex justify-content-end align-items-center" style="width: 92%;">
+                <ul class="list-unstyled text-start">
+                    <li class="d-flex mb-2 w-75">
+                        <img class="me-2" src="/icons/checkmark-blue.svg" alt="" width="25">
+                        <p class="fw-light">
+                            Entrena sin tener que moverte de tu hogar, no pierdas tiempo.
+                        </p>
+                    </li>
+                    <li class="d-flex mb-2 w-75">
+                        <img class="me-2" src="/icons/checkmark-green.svg" alt="" width="25">
+                        <p class="fw-light">
+                            Ahorras dinero en combustible y en planes de gimnasio que no asistes.
+                        </p>
+                    </li>
+                    <li class="d-flex mb-2 w-75">
+                        <img class="me-2" src="/icons/checkmark-blue.svg" alt="" width="25">
+                        <p class="fw-light">
+                            Sé el propio dueño de tu tiempo, la hora de entrenar la defines tú.
+                        </p>
+                    </li>
+                    <li class="d-flex w-75">
+                        <img class="me-2" src="/icons/checkmark-green.svg" alt="" width="25">
+                        <p class="fw-light">
+                            Asesoramiento integral: Coaching, actividad física y nutrición.
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
