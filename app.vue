a47d28c675e776824cc2e9f0838019b7a3178837
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-06-29 20:23:45 -0400

    add poppins

--- app.vue
+++ app.vue
@@ -4,8 +4,9 @@
   </NuxtLayout>
 </template>
 <style lang="scss">
-  @import url('https://fonts.cdnfonts.com/css/tellural');
 
+  @import url('https://fonts.cdnfonts.com/css/tellural');
+  @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
   body {
     font-family: 'Poppins', sans-serif;
   }
