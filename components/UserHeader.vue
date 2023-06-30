e5925ccd35ead9cd08d7a3c85280ded79d6e3057
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-06-30 18:19:54 -0400

    add references to other pages

--- components/UserHeader.vue
+++ components/UserHeader.vue
@@ -59,23 +59,22 @@
             <div class="sidebar" style="position: fixed; left: 0; width: 19vw; height: 100vh; background: white; box-shadow: 0px 0px 27px -6px rgba(0, 0, 0, 0.20); z-index: 0!important;">
                 <ul class="nav nav-pills flex-column py-4 align-items-betweeen"  style="margin-left: 1.5vw; height: 100%; ">
                     <div class="">
-
                         <li class="nav-item">
-                            <nuxt-link to="/" class="nav-link  active d-flex mx-2 gap-2" aria-current="page">
+                            <nuxt-link to="/user/main/userInicio" class="nav-link   d-flex mx-2 gap-2" >
                               <svg xmlns="http://www.w3.org/2000/svg" width="20" class="ionicon" viewBox="0 0 512 512"><path d="M261.56 101.28a8 8 0 00-11.06 0L66.4 277.15a8 8 0 00-2.47 5.79L63.9 448a32 32 0 0032 32H192a16 16 0 0016-16V328a8 8 0 018-8h80a8 8 0 018 8v136a16 16 0 0016 16h96.06a32 32 0 0032-32V282.94a8 8 0 00-2.47-5.79z"/><path d="M490.91 244.15l-74.8-71.56V64a16 16 0 00-16-16h-48a16 16 0 00-16 16v32l-57.92-55.38C272.77 35.14 264.71 32 256 32c-8.68 0-16.72 3.14-22.14 8.63l-212.7 203.5c-6.22 6-7 15.87-1.34 22.37A16 16 0 0043 267.56L250.5 69.28a8 8 0 0111.06 0l207.52 198.28a16 16 0 0022.59-.44c6.14-6.36 5.63-16.86-.76-22.97z"/></svg>
                               <p class="text-sidebar">Inicio</p>
                             </nuxt-link>
     
                         </li>
                         <li class="nav-item">
-                            <nuxt-link to="/" class="nav-link  d-flex mx-2 gap-2" >
+                            <nuxt-link to="/user/main/userSesiones" class="nav-link  d-flex mx-2 gap-2" >
                               <svg xmlns="http://www.w3.org/2000/svg" width="20" class="ionicon" viewBox="0 0 512 512"><path d="M256 48C141.31 48 48 141.31 48 256s93.31 208 208 208 208-93.31 208-208S370.69 48 256 48zm108.25 138.29l-134.4 160a16 16 0 01-12 5.71h-.27a16 16 0 01-11.89-5.3l-57.6-64a16 16 0 1123.78-21.4l45.29 50.32 122.59-145.91a16 16 0 0124.5 20.58z"/></svg>
                               <p class="text-sidebar">Sesiones agendadas</p>
                             </nuxt-link>
     
                         </li>
                         <li class="nav-item">
-                            <nuxt-link to="/" class="nav-link  d-flex mx-2 gap-2" >
+                            <nuxt-link to="/user/main/userAgendar" class="nav-link  d-flex mx-2 gap-2" >
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" class="ionicon" viewBox="0 0 512 512"><path d="M480 128a64 64 0 00-64-64h-16V48.45c0-8.61-6.62-16-15.23-16.43A16 16 0 00368 48v16H144V48.45c0-8.61-6.62-16-15.23-16.43A16 16 0 00112 48v16H96a64 64 0 00-64 64v12a4 4 0 004 4h440a4 4 0 004-4zM32 416a64 64 0 0064 64h320a64 64 0 0064-64V179a3 3 0 00-3-3H35a3 3 0 00-3 3zm344-208a24 24 0 11-24 24 24 24 0 0124-24zm0 80a24 24 0 11-24 24 24 24 0 0124-24zm-80-80a24 24 0 11-24 24 24 24 0 0124-24zm0 80a24 24 0 11-24 24 24 24 0 0124-24zm0 80a24 24 0 11-24 24 24 24 0 0124-24zm-80-80a24 24 0 11-24 24 24 24 0 0124-24zm0 80a24 24 0 11-24 24 24 24 0 0124-24zm-80-80a24 24 0 11-24 24 24 24 0 0124-24zm0 80a24 24 0 11-24 24 24 24 0 0124-24z"/></svg>
     
                               <p class="text-sidebar">Agendar Sesión</p>
@@ -159,13 +158,9 @@
       p{
         color: white;
       }
-
-    }
-    .active{
       background: #0EB3E0!important;
-      border-radius: 10px;
-      padding: 10px;
     }
+
     .nav-link:not([aria-current="page"]) {
       svg {
         fill:gray;
