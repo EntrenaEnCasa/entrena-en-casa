d8e4bd471fdaf259ca2f01fe076c625232777528
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-06-30 21:37:22 -0400

    add basic footer and add to default layout

---
+++ components/Footer.vue
@@ -0,0 +1,31 @@
+<template>
+    <div style="background-color: #0EB3E0">
+        <div class="py-5 container d-flex justify-content-between">
+            <img src="/logo-white.png" width="150" alt="">
+            <div class="d-flex gap-4 text-white">
+                <div>
+                    <h5>Contacto</h5>
+                    <div class="">
+                        <p>Correo electrónico</p>
+                        <p>entrenaencasa0@gmail.com</p>
+                    </div>
+                    <div>
+                        <p>Teléfono</p>
+                        <p>+56 9 7137 0313</p>
+                    </div>
+                </div>
+                <div>
+                    <h5>Redes sociales</h5>
+                    <div>
+                        <p>Facebook</p>
+                        <p>-</p>
+                    </div>
+                    <div>
+                        <p>Instagram</p>
+                        <p>@-</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
