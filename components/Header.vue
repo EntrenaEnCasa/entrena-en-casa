978ba4e540b829515106f36cbf4afb2fa98f1bb4
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-07-24 12:51:58 -0400

    update header to use tailwind

--- components/Header.vue
+++ components/Header.vue
@@ -1,40 +1,85 @@
 <template>
-    <nav class="navbar navbar-expand-lg bg-gradiente">
-        <div class="container w-100">
-            <div class="flex-grow-1">
-                <a class="navbar-brand" href="#">
-                    <img src="/logo-horizontal.png" class="object-fit-contain" alt="Bootstrap" width="150" height="50">
-                </a>
-            </div>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav gap-2 mb-2 mb-lg-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link " href="#">Sobre Nosotros</a>
-                    </li>
-                    <li class="nav-item ">
-                        <a class="nav-link " href="#">Planes</a>
-                    </li>
-                </ul>
-                <div class="ms-auto">
-                    <ul class="navbar-nav justify-content-center me-auto mb-2 mb-lg-0">
-                        <li class="nav-item p-2">
-                            <nuxt-link class="nav-link " to="/user/auth/register">Registro</nuxt-link>
-                        </li>
-                        <li class="nav-item p-2">
-                            <nuxt-link to="/user/auth/login"> 
-                                <button type="button" @click="" class="btn btn-outline-success">Iniciar sesión</button>
-                            </nuxt-link>
-                        </li>
-                    </ul>
-                </div>
+    <nav class="bg-gradiente">
+        <div class="mx-auto max-w-7xl px-2 lg:px-8">
+            <div class="relative flex h-20 items-center justify-between">
+                <div class="absolute inset-y-0 left-0 flex items-center lg:hidden">
+                    <!-- Mobile menu button-->
+                    <button type="button"
+                        class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-secondary hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                        aria-controls="mobile-menu" aria-expanded="false" @click="toggleMenu">
+                        <span class="sr-only">Open main menu</span>
+                        <!--
+            Icon when menu is closed.
 
+            Menu open: "hidden", Menu closed: "block"
+          -->
+                        <svg class="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                            aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round"
+                                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                        </svg>
+                        <!--
+            Icon when menu is open.
+
+            Menu open: "block", Menu closed: "hidden"
+          -->
+                        <svg class="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                            aria-hidden="true">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+                <div class="flex flex-1 items-center justify-center lg:items-stretch lg:justify-between">
+                    <div class="flex flex-shrink-0 items-center">
+                        <img class="h-12 w-auto" src="/logo-horizontal.png" alt="Logo entrena en casa">
+                    </div>
+                    <div class="hidden lg:flex items-center">
+                        <div class="flex space-x-4">
+                            <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                            <a href="#" class="text-secondary rounded-md px-3 py-2 font-medium"
+                                aria-current="page">Inicio</a>
+                            <a href="#"
+                                class="text-gray-600 hover:bg-secondary hover:text-white rounded-md px-3 py-2 font-medium">Sobre
+                                Nosotros</a>
+                            <a href="#"
+                                class="text-gray-600 hover:bg-secondary hover:text-white rounded-md px-3 py-2 font-medium">Planes</a>
+                        </div>
+                    </div>
+                    <div class="hidden lg:flex items-center font-medium text-gray-500 space-x-2">
+                        <nuxt-link to="/user/auth/register" class="px-3 py-2  rounded-xl">
+                            Registrarse
+                        </nuxt-link>
+                        <nuxt-link to="/user/auth/login" class="px-7 py-2 border-2 border-secondary rounded-3xl">
+                            Iniciar sesión
+                        </nuxt-link>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Mobile menu, show/hide based on menu state. -->
+        <div class="lg:hidden bg-[#E1F7FF]" id="mobile-menu" :class="menuOpen ? 'block' : 'hidden'">
+            <div class="space-y-1 px-2 pb-3 pt-2">
+                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+                <nuxt-link to="/"
+                    class="text-gray-600 hover:bg-secondary hover:text-white block rounded-md px-3 py-2 text-base font-medium">Inicio</nuxt-link>
+                <nuxt-link to="/"
+                    class="text-gray-600 hover:bg-secondary hover:text-white block rounded-md px-3 py-2 text-base font-medium">Sobre
+                    Nosotros</nuxt-link>
+                <nuxt-link to="/"
+                    class="text-gray-600 hover:bg-secondary hover:text-white block rounded-md px-3 py-2 text-base font-medium">
+                    Planes
+                </nuxt-link>
+                <div class="space-y-1 py-5">
+                    <nuxt-link to="/user/auth/register"
+                        class="text-secondary hover:bg-secondary hover:text-white block rounded-md px-3 py-2 text-base font-medium">
+                        Registrarse
+                    </nuxt-link>
+                    <nuxt-link to="/user/auth/login"
+                        class="text-gray-600 hover:bg-secondary border-secondary hover:text-white block rounded-md px-3 py-2 text-base font-medium">
+                        Iniciar sesión
+                    </nuxt-link>
+                </div>
             </div>
         </div>
     </nav>
@@ -45,3 +90,14 @@
     /* background: linear-gradient(136deg, rgba(198, 243, 255, 0.15) 0%, rgba(113, 212, 240, 0.15) 64.54%, rgba(0, 142, 181, 0.15) 100%); */
 }
 </style>
+<script setup>
+
+import { ref } from 'vue'
+
+const menuOpen = ref(false);
+
+const toggleMenu = () => {
+    menuOpen.value = !menuOpen.value;
+}
+
+</script>
