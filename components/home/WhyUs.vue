7f284364abe4e744f1fc4273a01fc2ace58b76e0
Gonzalo Bruna <catalina.schencke.p@mail.pucv.cl>
2023-07-25 16:21:37 -0400

    move Why Us component to tailwind

--- components/home/WhyUs.vue
+++ components/home/WhyUs.vue
@@ -1,33 +1,35 @@
 <template>
-    <div class="text-center" style="margin-top: 100px; margin-bottom: 20px;">
-        <p style="letter-spacing: .3rem;">BENEFICIOS</p>
-        <h2 class="fw-medium my-2 fs-1">¿Por qué Entrena en Casa?</h2>
-        <p class="fw-light">Entrena en Casa es la nueva forma de ejercitarse y vivir de forma más saludable.</p>
-        <div class="d-flex my-5 justify-between">
-            <img class="position-absolute" src="/home/woman-exercising.png" alt="woman exercising" width="500">
-            <div class="d-flex justify-content-end align-items-center" style="width: 92%;">
-                <ul class="list-unstyled text-start">
-                    <li class="d-flex mb-2 w-75">
-                        <img class="me-2" src="/icons/checkmark-blue.svg" alt="" width="25">
-                        <p class="fw-light">
+    <div style="margin-top: 100px; margin-bottom: 20px;">
+        <div class="text-center">
+            <h3 class=" tracking-[.3rem]">BENEFICIOS</h3>
+            <h2 class="font-medium my-3 text-4xl">¿Por qué Entrena en Casa?</h2>
+            <p class="font-light">Entrena en Casa es la nueva forma de ejercitarse y vivir de forma más saludable.</p>
+        </div>
+        <div class="flex my-10 justify-between">
+            <img class="absolute" src="/home/woman-exercising.png" alt="woman exercising" width="500">
+            <div class="flex justify-end items-center w-11/12 mx-auto">
+                <ul class="text-start space-y-2 flex flex-col items-center">
+                    <li class="flex w-3/4">
+                        <img class="mr-2" src="/icons/checkmark-blue.svg" alt="" width="25">
+                        <p class="font-light">
                             Entrena sin tener que moverte de tu hogar, no pierdas tiempo.
                         </p>
                     </li>
-                    <li class="d-flex mb-2 w-75">
-                        <img class="me-2" src="/icons/checkmark-green.svg" alt="" width="25">
-                        <p class="fw-light">
+                    <li class="flex w-3/4">
+                        <img class="mr-2" src="/icons/checkmark-green.svg" alt="" width="25">
+                        <p class="font-light">
                             Ahorras dinero en combustible y en planes de gimnasio que no asistes.
                         </p>
                     </li>
-                    <li class="d-flex mb-2 w-75">
-                        <img class="me-2" src="/icons/checkmark-blue.svg" alt="" width="25">
-                        <p class="fw-light">
+                    <li class="flex w-3/4">
+                        <img class="mr-2" src="/icons/checkmark-blue.svg" alt="" width="25">
+                        <p class="font-light">
                             Sé el propio dueño de tu tiempo, la hora de entrenar la defines tú.
                         </p>
                     </li>
-                    <li class="d-flex w-75">
-                        <img class="me-2" src="/icons/checkmark-green.svg" alt="" width="25">
-                        <p class="fw-light">
+                    <li class="flex w-3/4">
+                        <img class="mr-2" src="/icons/checkmark-green.svg" alt="" width="25">
+                        <p class="font-light">
                             Asesoramiento integral: Coaching, actividad física y nutrición.
                         </p>
                     </li>
