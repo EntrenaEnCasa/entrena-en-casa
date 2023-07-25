7e60ed533940d533ab72fb69293c9600954a6c51
Gonzalo Bruna <catalina.schencke.p@mail.pucv.cl>
2023-07-25 17:44:30 -0400

    move plans component to tailwind

--- components/home/Plans.vue
+++ components/home/Plans.vue
@@ -1,99 +1,82 @@
 <template>
     <div class="bg-gradiente" style="padding-top: 100px; padding-bottom: 70px;">
-        <div class="container">
-            <div class="row">
-                <div class="col-8">
-                    <div class="d-flex flex-column" style="gap: 2rem">
-                        <div class="px-4 py-3 border mx-auto bg-white position-relative" style="width: 85%;">
-                            <p class="fw-semibold" style="letter-spacing: .2rem;">ENTRENAMIENTO FUNCIONAL</p>
-                            <p class="fw-semibold fs-7" style="letter-spacing: .15rem;">BORDE COSTERO</p>
-                            <p class="text-center my-2">
-                                Entrenamiento una vez por semana
-                                <br>
-                                Cada sábado a las 12:00hrs
-                            </p>
-                            <div class="d-flex justify-content-between" style="color: #0EB3E0">
-                                <p class="fst-italic">Apto para todo tipo de personas</p>
-                                <button class="border-0 bg-transparent tellural" style="color: #0EB3E0">Contratar</button>
-                            </div>
-                            <div class="position-absolute" style="top: -22px; right: -40px;">
-                                <span class="text-white fs-2 fw-semibold py-1 px-3 tellural rounded"
-                                    style="background-color: #0EB3E0">
-                                    $20.000
-                                </span>
-                            </div>
+        <div class="grid grid-cols-3">
+            <div class="col-span-2">
+                <div class="flex flex-col gap-8">
+                    <div class="px-4 py-3 border mx-auto bg-white relative w-10/12">
+                        <p class="font-semibold tracking-[.2rem]">ENTRENAMIENTO FUNCIONAL</p>
+                        <p class="font-semibold text-sm" style="letter-spacing: .15rem;">BORDE COSTERO</p>
+                        <p class="text-center my-2">
+                            Entrenamiento una vez por semana
+                            <br>
+                            Cada sábado a las 12:00hrs
+                        </p>
+                        <div class="flex justify-between text-secondary">
+                            <p class="italic">Apto para todo tipo de personas</p>
+                            <button class="border-none bg-transparent tellural text-secondary">Contratar</button>
                         </div>
-                        <div class="px-4 py-3 border mx-auto bg-white position-relative" style="width: 85%;">
-                            <p class="fw-semibold" style="letter-spacing: .2rem;">PLAN ESTANDAR</p>
-                            <p class="text-center my-2">
-                                Entrenamiento dos veces por semana
-                                <br>
-                                Acompañamiento el 100% de la sesión
-                                <br>
-                                modalidad <span style="color: #0EB3E0">online</span>
-                            </p>
-                            <div class="d-flex justify-content-end" style="color: #0EB3E0">
-                                <button class="border-0 bg-transparent tellural" style="color: #0EB3E0">Contratar</button>
-                            </div>
-                            <div class="position-absolute" style="top: -22px; right: -40px;">
-                                <span class="text-white fs-2 fw-semibold py-1 px-3 tellural rounded"
-                                    style="background-color: #0EB3E0">
-                                    $112.000
-                                </span>
-                            </div>
-                            <div class="position-absolute" style="  top: 40%; right: 8px; transform: translate(0, -100%);">
-                                <span class="fs-6 tellural" style="color: #0EB3E0">
-                                    $14.000/sesión
-                                </span>
-                            </div>
+                        <div class="absolute -top-4 -right-8">
+                            <span class="text-white text-3xl font-semibold py-2 px-4 tellural rounded-lg bg-secondary">
+                                $20.000
+                            </span>
                         </div>
-                        <div class="px-4 py-3 border mx-auto bg-white position-relative" style="width: 85%;">
-                            <p class="fw-semibold" style="letter-spacing: .2rem;">PLAN ESTANDAR</p>
-                            <p class="fw-semibold fs-7" style="letter-spacing: .15rem;">VIÑA DEL MAR</p>
-                            <p class="text-center my-2">
-                                Entrenamiento dos veces por semana
-                                <br>
-                                Sesión individual
-                                <br>
-                                modalidad <span style="color: #0EB3E0">presencial</span>
-                            </p>
-                            <div class="d-flex justify-content-end">
-                                <button class="border-0 bg-transparent tellural" style="color: #0EB3E0">Contratar</button>
-                            </div>
-                            <div class="position-absolute" style="top: -22px; right: -40px;">
-                                <span class="text-white fs-2 fw-semibold py-1 px-3 tellural  rounded"
-                                    style="background-color: #0EB3E0">
-                                    $140.000
-                                </span>
-                            </div>
-                            <div class="position-absolute" style="  top: 40%; right: 8px; transform: translate(0, -100%);">
-                                <span class="fs-6 tellural" style="color: #0EB3E0">
-                                    $17.000/sesión
-                                </span>
-                            </div>
+                    </div>
+                    <div class="px-4 py-3 border mx-auto bg-white relative w-10/12">
+                        <p class="font-semibold tracking-[.2rem]">PLAN ESTÁNDAR</p>
+                        <p class="text-center my-2">
+                            Entrenamiento dos veces por semana
+                            <br>
+                            Acompañamiento el 100% de la sesión
+                            <br />
+                            modalidad <span class="text-secondary">online</span>
+                        </p>
+                        <div class="flex justify-end text-secondary">
+                            <button class="border-none bg-transparent tellural text-secondary">Contratar</button>
+                        </div>
+                        <div class="absolute -top-4 -right-8">
+                            <span class="text-white text-3xl font-semibold py-2 px-4 tellural rounded-lg bg-secondary">
+                                $112.000
+                            </span>
+                        </div>
+                        <div class="absolute top-[15%] right-[8px] translate-y-full">
+                            <span class="tellural text-secondary">
+                                $14.000/sesión
+                            </span>
+                        </div>
+                    </div>
+                    <div class="px-4 py-3 border mx-auto bg-white relative w-10/12">
+                        <p class="font-semibold tracking-[.2rem]">PLAN ESTÁNDAR</p>
+                        <p class="font-semibold text-sm" style="letter-spacing: .15rem;">VIÑA DEL MAR</p>
+                        <p class="text-center my-2">
+                            Entrenamiento dos veces por semana
+                            <br>
+                            Sesión individual
+                            <br />
+                            Modalidad <span class="text-secondary">presencial</span>
+                        </p>
+                        <div class="flex justify-end text-secondary">
+                            <button class="border-none bg-transparent tellural text-secondary">Contratar</button>
+                        </div>
+                        <div class="absolute -top-4 -right-8">
+                            <span class="text-white text-3xl font-semibold py-2 px-4 tellural rounded-lg bg-secondary">
+                                $140.000
+                            </span>
+                        </div>
+                        <div class="absolute top-[15%] right-[8px] translate-y-full">
+                            <span class="tellural text-secondary">
+                                $17.500/sesión
+                            </span>
                         </div>
                     </div>
                 </div>
-                <div class="col-4 d-flex flex-column justify-content-center align-items-center">
-                    <p class="fs-7" style="letter-spacing: .2rem;">SERVICIOS</p>
-                    <h5 class="fs-1">Nuestros planes</h5>
-                    <p class="fs-7" style="letter-spacing: .2rem;">MÁS VENDIDOS</p>
-                    <img src=" /home/runner.png" alt="" width="350">
-                </div>
+            </div>
+            <div class="col-span-1 flex flex-col justify-center items-center space-y-2">
+                <p class="text-sm tracking-[.2rem]">SERVICIOS</p>
+                <h5 class="text-4xl font-medium">Nuestros planes</h5>
+                <p class="text-sm tracking-[.2rem]">MÁS VENDIDOS</p>
+                <img src=" /home/runner.png" alt="" width="350">
             </div>
         </div>
-        <!-- <div class="d-flex">
-            <div>
-                <div class="p-2 border">
-                    <p class="fw-medium" style="letter-spacing: .2rem;">ENTRENAMIENTO FUNCIONAL</p>
-                    <p class="fw-medium fs-7" style="letter-spacing: .15rem;">BORDE COSTERO</p>
-                    <p></p>
-                </div>
-            </div>
-            <div>
-                <img src=" /home/runner.png" alt="" width="250">
-            </div>
-        </div> -->
     </div>
 </template>
 <style scoped>
