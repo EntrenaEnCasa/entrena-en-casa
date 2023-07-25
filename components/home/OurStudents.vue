b196a63be4d0babc1dfc3a9e0ff3d2df95903fe3
Gonzalo Bruna <catalina.schencke.p@mail.pucv.cl>
2023-07-25 18:14:01 -0400

    move ourStudents component to tailwind

--- components/home/OurStudents.vue
+++ components/home/OurStudents.vue
@@ -1,40 +1,34 @@
 <template>
-    <div class="bg-gradiente position-relative" style="padding-top: 100px; padding-bottom: 100px; z-index: -2;">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col text-center">
-                    <p class="fs-7" style="letter-spacing: .15rem;">TESTIOMONIOS</p>
-                    <h2 class="fw-medium fs-1">¿Qué es Entrena en Casa?</h2>
-                    <p>Algunos comentarios de alumnos que han utilizado Entrena en Casa</p>
+    <div class="bg-gradiente relative" style="padding-top: 100px; padding-bottom: 100px; z-index: -2;">
+        <div class="grid grid-cols-2 items-center w-11/12 mx-auto">
+            <div class="text-center">
+                <p class="text-sm tracking-[.15rem]">TESTIOMONIOS</p>
+                <h2 class="font-medium text-4xl ">Nuestros alumnos</h2>
+                <p class="w-9/12 mx-auto">Algunos comentarios de alumnos que han utilizado Entrena en Casa</p>
+            </div>
+            <div class="shadow p-4 flex flex-col bg-white gap-2 relative rounded-2xl">
+                <div class="self-end">
+                    <img src="/icons/stars-group.svg" alt="">
                 </div>
-                <div class="col">
-                    <div class="shadow-sm p-4 d-flex flex-column bg-white gap-2 position-relative"
-                        style="border-radius: 15px;">
-                        <div class="align-self-end">
-                            <img src="/icons/stars-group.svg" alt="">
-                        </div>
-                        <div class="align-self-start">
-                            <img src="/icons/comillas.png" width="40" alt="">
-                        </div>
-                        <div class="align-self-center" style="width: 90%;">
-                            <p style="text-align: justify;">
-                                Desde que inicie mi entrenamiento me siento excelente, más activa, liviana y cada día más
-                                fuerte y delgada. Además de eso mentalmente sintiéndome feliz con los resultados que he
-                                obtenido.
-                                <br>
-                                Vamos equipo el grito de guerra de Jorge, vamos que si puedes, eso nos incentiva a ser cada
-                                vez más fuertes, rápidos. El compromiso del entrenamiento y del grupo es un 100%.
-                            </p>
-                        </div>
-                        <div class="align-self-end text-end">
-                            <p class="fs-5 fw-semibold">- María Gutierrez</p>
-                            <p class="">Doctora</p>
-                        </div>
-                        <div class="w-100 h-100 position-absolute"
-                            style="border-radius: 15px; background: #f9f9f9; z-index: -1; bottom: -15%; right: -8%">
-
-                        </div>
-                    </div>
+                <div class="self-start">
+                    <img src="/icons/comillas.png" width="40" alt="">
+                </div>
+                <div class="self-center w-11/12">
+                    <p class="text-justify">
+                        Desde que inicie mi entrenamiento me siento excelente, más activa, liviana y cada día más
+                        fuerte y delgada. Además de eso mentalmente sintiéndome feliz con los resultados que he
+                        obtenido.
+                        <br>
+                        Vamos equipo el grito de guerra de Jorge, vamos que si puedes, eso nos incentiva a ser cada
+                        vez más fuertes, rápidos. El compromiso del entrenamiento y del grupo es un 100%.
+                    </p>
+                </div>
+                <div class="self-end text-end">
+                    <p class="text-lg font-semibold">- María Gutierrez</p>
+                    <p>Doctora</p>
+                </div>
+                <div class="w-full h-full absolute"
+                    style="border-radius: 15px; background: #f9f9f9; z-index: -1; bottom: -15%; right: -8%">
                 </div>
             </div>
         </div>
