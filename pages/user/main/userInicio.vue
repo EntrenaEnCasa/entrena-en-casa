eda6710db9f59deeded8f8a54b7a840f3ea7fe4e
Gonzalo Bruna <catalina.schencke.p@mail.pucv.cl>
2023-07-28 13:35:28 -0400

    update userInicio component to use tailwind

--- pages/user/main/userInicio.vue
+++ pages/user/main/userInicio.vue
@@ -1,87 +1,48 @@
 <template >
-    <div class="userInicio py-5 px-5 column-gap-5">
-        <div class="row row-gap-4 ">
-            <div class="col-md-6">
-                <div class="card cardInicio">
-                    <div class="card-body">
-                        <h5 class="card-title">Últimas sesiones</h5>
-                    </div>
+    <div class="bg-[#F7FAFA] p-6 sm:p-8 space-y-4">
+        <div class="grid grid-cols-2 gap-x-4">
+            <div class="w-full h-72 py-4 px-6 rounded-2xl border border-zinc-200"
+                style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
+                <div>
+                    <h5 class="text-lg text-[#949494]">Últimas sesiones</h5>
+                </div>
+                <div class="mt-5">
+                    <p class="text-lg text-[#949494]">No hay estadísticas disponibles actualmente.</p>
                 </div>
             </div>
-            <div class="col-md-6">
-                <div class="card cardInicio">
-                    <div class="card-body">
-                        <h5 class="card-title">Próximas sesiones</h5>
-                    </div>
+            <div class="w-full h-72 py-4 px-6 rounded-2xl border border-zinc-200"
+                style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
+                <div>
+                    <h5 class="text-lg text-[#949494]">Próximas sesiones</h5>
+                </div>
+                <div class="mt-5">
+                    <p class="text-lg text-[#949494]">No hay estadísticas disponibles actualmente.</p>
                 </div>
             </div>
-            <div class="col-md-12">
-                <div class="card cardInicio">
-                    <div class="card-body cartaGrande">
-                        <div class="titulo">
-                            <h5 class="card-title">Estadísticas</h5>
-                        </div>
-                        <div class="texto">
-                            <p class="card-text align-middle mt-5 pt-5">No hay estadísticas disponibles actualmente.</p>
-                        </div>
-                    </div>
+        </div>
+        <div>
+            <div class="w-full h-72 py-4 px-6 rounded-2xl border border-zinc-200"
+                style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
+                <div>
+                    <h5 class="text-lg text-[#949494]">Estadísticas</h5>
+                </div>
+                <div class="mt-5">
+                    <p class="text-lg text-[#949494]">No hay estadísticas disponibles actualmente.</p>
                 </div>
             </div>
         </div>
     </div>
 </template>
 
+<style scoped lang="scss">
+.userInicio {
+    background: linear-gradient(135deg, rgba(153, 208, 223, 0.10) 0%, rgba(255, 255, 255, 0.10) 0.01%, rgba(63, 136, 166, 0.10) 100%);
+    box-shadow: 0px -29px 89px 0px rgba(0, 0, 0, 0.10);
+}
+</style>
 
 <script setup>
 definePageMeta({
     layout: "user",
 });
 </script>
-
-
-<style scoped lang="scss">
-$color_enfasis: #0EB3E0;
-$color_principal: #505050;
-$color_secondary: #C3C3C3;
-$extra-color: #949494;
-$green: #B5CD13;
-$red: #FF3A3A;
-
-$font_family_primary: 'Poppins', sans-serif;
-$font_family_Secondary: 'tellural', sans-serif;
-
-.userInicio {
-    font-family: $font_family_primary;
-    background: linear-gradient(135deg, rgba(153, 208, 223, 0.10) 0%, rgba(255, 255, 255, 0.10) 0.01%, rgba(63, 136, 166, 0.10) 100%);
-    box-shadow: 0px -29px 89px 0px rgba(0, 0, 0, 0.10);
-}
-
-.elemento {
-    background-color: red;
-}
-
-.card.cardInicio {
-    width: 100%;
-    height: 300px;
-    border-radius: 20px;
-    border: 1px solid #E8E8E8;
-    box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);
-}
-
-.card-title {
-    color: $extra-color;
-    font-size: 20px;
-}
-
-.card-text {
-    color: $extra-color;
-    font-size: 25px;
-    text-align: center;
-}
-
-.texto {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-}
-</style>
