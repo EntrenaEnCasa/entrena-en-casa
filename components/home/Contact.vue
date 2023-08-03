21842b88061bd630e92e654b2bf3c5207d2d06a4
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-08-03 19:18:38 -0400

    fix contact not being correctly sized

--- components/home/Contact.vue
+++ components/home/Contact.vue
@@ -2,23 +2,14 @@
     <div style="margin-top: 60px; margin-bottom: 60px">
         <div class="flex flex-col items-center justify-center">
             <p class="text-sm tracking-[.15rem]">CONTACTO</p>
-            <h2 class="text-4xl font-medium">¿Tienes dudas? ¡Contactanos!</h2>
-            <div class="flex gap-4 mt-5 space-x-5">
-                <div class="text-2xl flex items-center gap-2 text-secondary">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512">
-                        <rect x="48" y="96" width="416" height="320" rx="40" ry="40" fill="none" stroke="currentColor"
-                            stroke-linecap="round" stroke-linejoin="round" stroke-width="32" />
-                        <path fill="none" stroke="#0EB3E0" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"
-                            d="M112 160l144 112 144-112" />
-                    </svg>
-                    <p>entrenaencasa0@gmail.com</p>
+            <h2 class="text-4xl font-medium text-center">¿Tienes dudas? ¡Contactanos!</h2>
+            <div class="flex flex-col md:flex-row gap-4 mt-5 space-x-5">
+                <div class="flex items-center gap-x-3 text-secondary">
+                    <Icon class="text-3xl" name="fa6-solid:envelope" />
+                    <p class="text-xl">entrenaencasa0@gmail.com</p>
                 </div>
                 <div class="text-2xl flex items-center gap-2 text-secondary">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512">
-                        <path
-                            d="M451 374c-15.88-16-54.34-39.35-73-48.76-24.3-12.24-26.3-13.24-45.4.95-12.74 9.47-21.21 17.93-36.12 14.75s-47.31-21.11-75.68-49.39-47.34-61.62-50.53-76.48 5.41-23.23 14.79-36c13.22-18 12.22-21 .92-45.3-8.81-18.9-32.84-57-48.9-72.8C119.9 44 119.9 47 108.83 51.6A160.15 160.15 0 0083 65.37C67 76 58.12 84.83 51.91 98.1s-9 44.38 23.07 102.64 54.57 88.05 101.14 134.49S258.5 406.64 310.85 436c64.76 36.27 89.6 29.2 102.91 23s22.18-15 32.83-31a159.09 159.09 0 0013.8-25.8C465 391.17 468 391.17 451 374z"
-                            fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32" />
-                    </svg>
+                    <Icon class="text-3xl" name="fa6-solid:phone" />
                     <p>+56 9 7137 0313</p>
                 </div>
             </div>
