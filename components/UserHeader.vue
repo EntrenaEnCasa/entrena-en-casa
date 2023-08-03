44dd69e12803d32c5bbabb28abf23fba984b1f7e
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-08-03 11:07:04 -0400

    move sidebar into its own component

--- components/UserHeader.vue
+++ components/UserHeader.vue
@@ -93,56 +93,7 @@
         </div>
     </nav>
 
-    <aside id="logo-sidebar"
-        class="fixed top-0 left-0 z-40 w-64 h-screen pt-24 transition-transform -translate-x-full bg-white border-r border-gray-200 lg:translate-x-0"
-        :class="sidebarOpen ? 'translate-x-0' : '-translate-x-full'" aria-label="Sidebar">
-        <div class="h-full px-3 pb-4 overflow-y-auto bg-white">
-            <ul class="space-y-2 font-medium">
-                <li>
-                    <router-link @click="sidebarOpen = false" to="/user/main/userInicio"
-                        class="flex items-center p-3 text-gray-500 rounded-lg hover:bg-secondary group">
-                        <Icon name="fa6-solid:house-chimney" class="w-5 h-5 text-gray-500 group-hover:text-white" />
-                        <span class="flex-1 ml-3 whitespace-nowrap group-hover:text-white">Inicio</span>
-                    </router-link>
-                </li>
-                <li>
-                    <router-link @click="sidebarOpen = false" to="/user/main/userSesiones"
-                        class="flex items-center p-3 text-gray-500 rounded-lg hover:bg-secondary group">
-                        <Icon name="fa6-solid:circle-check" class="w-5 h-5 text-gray-500 group-hover:text-white" />
-                        <span class="flex-1 ml-3 whitespace-nowrap group-hover:text-white">Sesiones agendadas</span>
-                    </router-link>
-                </li>
-                <li>
-                    <router-link @click="sidebarOpen = false" to="/user/main/userAgendar"
-                        class="flex items-center p-3 text-gray-500 rounded-lg hover:bg-secondary group">
-                        <Icon name="fa6-solid:calendar-days" class="w-5 h-5 text-gray-500 group-hover:text-white" />
-                        <span class="flex-1 ml-3 whitespace-nowrap group-hover:text-white">Agendar sesión</span>
-                    </router-link>
-                </li>
-                <!-- <li>
-                    <router-link @click="sidebarOpen = false" to="/user/main/userRecom"
-                        class="flex items-center p-3 text-gray-500 rounded-lg hover:bg-secondary group">
-                        <Icon name="fa6-solid:ranking-star" class="w-5 h-5 text-gray-500 group-hover:text-white" />
-                        <span class="flex-1 ml-3 whitespace-nowrap group-hover:text-white">Recomendaciones</span>
-                    </router-link>
-                </li>
-                <li>
-                    <router-link @click="sidebarOpen = false" to="#"
-                        class="flex items-center p-3 text-gray-500 rounded-lg hover:bg-secondary group">
-                        <Icon name="fa6-solid:chart-column" class="w-5 h-5 text-gray-500 group-hover:text-white" />
-                        <span class="flex-1 ml-3 whitespace-nowrap group-hover:text-white">Estadísticas</span>
-                    </router-link>
-                </li>
-                <li>
-                    <router-link @click="sidebarOpen = false" to="/user/main/userCredits"
-                        class="flex items-center p-3 text-gray-500 rounded-lg hover:bg-secondary group">
-                        <Icon name="fa6-solid:money-bill" class="w-5 h-5 text-gray-500 group-hover:text-white" />
-                        <span class="flex-1 ml-3 whitespace-nowrap group-hover:text-white">Créditos</span>
-                    </router-link>
-                </li> -->
-            </ul>
-        </div>
-    </aside>
+    <UserSidebar ref="sidebar" />
 
     <div @click="sidebarOpen = false" class="mt-[4.5rem] lg:ml-64 transition-[background]"
         :class="{ 'bg-black/20 lg:bg-transparent': sidebarOpen }">
@@ -152,10 +103,12 @@
 
 <script setup>
 
+import { ref } from 'vue';
+
 const router = useRouter();
 
-const sidebarOpen = ref(false);
-const toggleSidebar = () => sidebarOpen.value = !sidebarOpen.value;
+const sidebar = ref(null);
+const toggleSidebar = () => sidebar.value.toggleSidebar();
 
 const userMenuOpen = ref(false);
 const toggleUserMenu = () => userMenuOpen.value = !userMenuOpen.value;
