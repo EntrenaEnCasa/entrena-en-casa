505467ceb57df5f44d4e4d7488d3241a5c8ca10a
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-08-02 21:03:59 -0400

    replace svg with nuxt icons

--- pages/user/main/userSesiones.vue
+++ pages/user/main/userSesiones.vue
@@ -1,52 +1,36 @@
 <template>
     <div class="py-5 px-5">
         <div class="row space-y-4 mx-5">
-            <div><h3>Sesión en curso</h3></div>
+            <div>
+                <h3>Sesión en curso</h3>
+            </div>
             <div>
                 <div class="w-full lg:h-15 py-4 px-6 rounded-2xl border border-zinc-200 lg:flex gap-6 items-center"
-                style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
-                
+                    style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
+
                     <div class="text-[#949494] text-sm">16:00hrs</div>
                     <div class="text-[#000000] text-lg font-medium text-center"><b>Tipo de sesión</b></div>
                     <div class="text-[#949494] text-base text-center">Nombre Profesional - Tipo Profesional</div>
-                    <div class="text-[#0EB3E0] text-2xl font-medium text-center"><a  href="#">Online</a></div>
+                    <div class="text-[#0EB3E0] text-2xl font-medium text-center"><a href="#">Online</a></div>
                     <div class="text-[#949494] text-base text-center gap-2">
                         <p>Calificar sesión</p>
                         <div class="flex place-content-center">
-                            <svg viewBox="0 0 78 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                d="M39 0L47.9806 27.6393H77.0423L53.5309 44.7214L62.5114 72.3607L39 55.2786L15.4886 72.3607L24.4691 44.7214L0.957741 27.6393H30.0194L39 0Z"
-                                fill="#949494" />
-                            </svg>
-                            <svg viewBox="0 0 78 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                d="M39 0L47.9806 27.6393H77.0423L53.5309 44.7214L62.5114 72.3607L39 55.2786L15.4886 72.3607L24.4691 44.7214L0.957741 27.6393H30.0194L39 0Z"
-                                fill="#949494" />
-                            </svg>
-                            <svg viewBox="0 0 78 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                d="M39 0L47.9806 27.6393H77.0423L53.5309 44.7214L62.5114 72.3607L39 55.2786L15.4886 72.3607L24.4691 44.7214L0.957741 27.6393H30.0194L39 0Z"
-                                fill="#949494" />
-                            </svg>
-                            <svg viewBox="0 0 78 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                d="M39 0L47.9806 27.6393H77.0423L53.5309 44.7214L62.5114 72.3607L39 55.2786L15.4886 72.3607L24.4691 44.7214L0.957741 27.6393H30.0194L39 0Z"
-                                fill="#949494" />
-                            </svg>
-                            <svg viewBox="0 0 78 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                d="M39 0L47.9806 27.6393H77.0423L53.5309 44.7214L62.5114 72.3607L39 55.2786L15.4886 72.3607L24.4691 44.7214L0.957741 27.6393H30.0194L39 0Z"
-                                fill="#949494" />
-                            </svg>
+                            <Icon class="text-[#949494]" name="heroicons:star-20-solid" />
+                            <Icon class="text-[#949494]" name="heroicons:star-20-solid" />
+                            <Icon class="text-[#949494]" name="heroicons:star-20-solid" />
+                            <Icon class="text-[#949494]" name="heroicons:star-20-solid" />
+                            <Icon class="text-[#949494]" name="heroicons:star-20-solid" />
                         </div>
                     </div>
                 </div>
             </div>
-            <div><h3>Sesiones próximas</h3></div>
+            <div>
+                <h3>Sesiones próximas</h3>
+            </div>
             <div>
                 <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
                     <div class="w-full py-4 px-6 rounded-2xl border border-zinc-200 gap-6 items-center space-y-3"
-                    style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
+                        style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
                         <div class="px-3">
                             <div class="grid grid-cols-2 gap-1">
                                 <div class="text-[#949494] text-sm text-left">
@@ -62,7 +46,7 @@
                         </div>
                     </div>
                     <div class="w-full py-4 px-6 rounded-2xl border border-zinc-200 gap-6 items-center space-y-5"
-                    style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
+                        style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
                         <div class="px-3">
                             <div class="grid grid-cols-2 gap-1">
                                 <div class="text-[#949494] text-sm text-left">
@@ -79,11 +63,13 @@
                     </div>
                 </div>
             </div>
-            <div><h3>Sesiones pasadas</h3></div>
+            <div>
+                <h3>Sesiones pasadas</h3>
+            </div>
             <div>
                 <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
                     <div class="w-full py-4 px-6 rounded-2xl border border-zinc-200 gap-6 items-center space-y-3 opacity-60"
-                    style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
+                        style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
                         <div class="px-3">
                             <div class="grid grid-cols-2 gap-1">
                                 <div class="text-[#949494] text-sm text-left">
@@ -95,41 +81,21 @@
                             </div>
                             <div class="text-[#000000] text-lg font-medium text-center"><b>Tipo de sesión</b></div>
                             <div class="text-[#949494] text-base text-center">Nombre Profesional - Tipo Profesional</div>
-                            <div class="text-[#0EB3E0] text-2xl font-medium text-center"><a  href="#">Online</a></div>
+                            <div class="text-[#0EB3E0] text-2xl font-medium text-center"><a href="#">Online</a></div>
                             <div class="text-[#949494] text-base text-center gap-2">
                                 <p>Calificar sesión</p>
                                 <div class="flex place-content-center">
-                                    <svg viewBox="0 0 78 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                        d="M39 0L47.9806 27.6393H77.0423L53.5309 44.7214L62.5114 72.3607L39 55.2786L15.4886 72.3607L24.4691 44.7214L0.957741 27.6393H30.0194L39 0Z"
-                                        fill="#949494" />
-                                    </svg>
-                                    <svg viewBox="0 0 78 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                        d="M39 0L47.9806 27.6393H77.0423L53.5309 44.7214L62.5114 72.3607L39 55.2786L15.4886 72.3607L24.4691 44.7214L0.957741 27.6393H30.0194L39 0Z"
-                                        fill="#949494" />
-                                    </svg>
-                                    <svg viewBox="0 0 78 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                        d="M39 0L47.9806 27.6393H77.0423L53.5309 44.7214L62.5114 72.3607L39 55.2786L15.4886 72.3607L24.4691 44.7214L0.957741 27.6393H30.0194L39 0Z"
-                                        fill="#949494" />
-                                    </svg>
-                                    <svg viewBox="0 0 78 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                        d="M39 0L47.9806 27.6393H77.0423L53.5309 44.7214L62.5114 72.3607L39 55.2786L15.4886 72.3607L24.4691 44.7214L0.957741 27.6393H30.0194L39 0Z"
-                                        fill="#949494" />
-                                    </svg>
-                                    <svg viewBox="0 0 78 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                        d="M39 0L47.9806 27.6393H77.0423L53.5309 44.7214L62.5114 72.3607L39 55.2786L15.4886 72.3607L24.4691 44.7214L0.957741 27.6393H30.0194L39 0Z"
-                                        fill="#949494" />
-                                    </svg>
+                                    <Icon class="text-[#949494]" name="heroicons:star-20-solid" />
+                                    <Icon class="text-[#949494]" name="heroicons:star-20-solid" />
+                                    <Icon class="text-[#949494]" name="heroicons:star-20-solid" />
+                                    <Icon class="text-[#949494]" name="heroicons:star-20-solid" />
+                                    <Icon class="text-[#949494]" name="heroicons:star-20-solid" />
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="w-full py-4 px-6 rounded-2xl border border-zinc-200 gap-6 items-center space-y-3 opacity-60"
-                    style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
+                        style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
                         <div class="px-3">
                             <div class="grid grid-cols-2 gap-1">
                                 <div class="text-[#949494] text-sm text-left">
@@ -141,40 +107,20 @@
                             </div>
                             <div class="text-[#000000] text-lg font-medium text-center"><b>Tipo de sesión</b></div>
                             <div class="text-[#949494] text-base text-center">Nombre Profesional - Tipo Profesional</div>
-                            <div class="text-[#0EB3E0] text-2xl font-medium text-center"><a  href="#">Online</a></div>
+                            <div class="text-[#0EB3E0] text-2xl font-medium text-center"><a href="#">Online</a></div>
                             <div class="text-[#949494] text-base text-center gap-2">
                                 <p>Calificar sesión</p>
                                 <div class="flex place-content-center">
-                                    <svg viewBox="0 0 78 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                        d="M39 0L47.9806 27.6393H77.0423L53.5309 44.7214L62.5114 72.3607L39 55.2786L15.4886 72.3607L24.4691 44.7214L0.957741 27.6393H30.0194L39 0Z"
-                                        fill="#949494" />
-                                    </svg>
-                                    <svg viewBox="0 0 78 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                        d="M39 0L47.9806 27.6393H77.0423L53.5309 44.7214L62.5114 72.3607L39 55.2786L15.4886 72.3607L24.4691 44.7214L0.957741 27.6393H30.0194L39 0Z"
-                                        fill="#949494" />
-                                    </svg>
-                                    <svg viewBox="0 0 78 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                        d="M39 0L47.9806 27.6393H77.0423L53.5309 44.7214L62.5114 72.3607L39 55.2786L15.4886 72.3607L24.4691 44.7214L0.957741 27.6393H30.0194L39 0Z"
-                                        fill="#949494" />
-                                    </svg>
-                                    <svg viewBox="0 0 78 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                        d="M39 0L47.9806 27.6393H77.0423L53.5309 44.7214L62.5114 72.3607L39 55.2786L15.4886 72.3607L24.4691 44.7214L0.957741 27.6393H30.0194L39 0Z"
-                                        fill="#949494" />
-                                    </svg>
-                                    <svg viewBox="0 0 78 73" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                        d="M39 0L47.9806 27.6393H77.0423L53.5309 44.7214L62.5114 72.3607L39 55.2786L15.4886 72.3607L24.4691 44.7214L0.957741 27.6393H30.0194L39 0Z"
-                                        fill="#949494" />
-                                    </svg>
+                                    <Icon class="text-[#949494]" name="heroicons:star-20-solid" />
+                                    <Icon class="text-[#949494]" name="heroicons:star-20-solid" />
+                                    <Icon class="text-[#949494]" name="heroicons:star-20-solid" />
+                                    <Icon class="text-[#949494]" name="heroicons:star-20-solid" />
+                                    <Icon class="text-[#949494]" name="heroicons:star-20-solid" />
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>    
+                </div>
             </div>
         </div>
     </div>
@@ -187,13 +133,10 @@ definePageMeta({
 </script>
 
 <style scoped lang="scss">
-
 svg {
     width: 20px;
     height: 20px;
     margin: 2px;
-    
+
 }
-
-
 </style>
