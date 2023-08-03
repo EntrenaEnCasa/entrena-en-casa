252feb9a0731c2fcd74ee5479bc8f76cd3d58657
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-08-03 19:58:54 -0400

    add bg to user dashboard content

--- layouts/user.vue
+++ layouts/user.vue
@@ -4,8 +4,10 @@
         <UserSidebar ref="sidebar" />
         <main @click="sidebar.sidebarOpen = false" class="mt-[4.5rem] lg:ml-64 transition-[background]">
             <div class="relative">
-                <slot></slot>
-                <div class="absolute top-0 w-full h-full min-h-[calc(100vh_-_4.5rem)] bg-black/10 backdrop-blur-[3px] lg:backdrop-blur-none lg:bg-transparent transition-all"
+                <div class="min-h-[calc(100vh_-_4.5rem)] bg-[#F7FAFA]">
+                    <slot></slot>
+                </div>
+                <div class="absolute top-0 w-full h-full bg-black/10 backdrop-blur-[3px] lg:backdrop-blur-none lg:bg-transparent transition-all"
                     :class="{ 'hidden': !sidebar?.sidebarOpen }">
                 </div>
             </div>
