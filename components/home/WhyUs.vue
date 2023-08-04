ba31be5760a018d4d5579cbbed550af9c1691199
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-08-04 13:55:57 -0400

    make home why us component responsive

--- components/home/WhyUs.vue
+++ components/home/WhyUs.vue
@@ -1,13 +1,13 @@
 <template>
-    <div style="margin-top: 100px; margin-bottom: 20px;">
-        <div class="text-center">
+    <div class="my-5 md:mt-36 lg:mt-32">
+        <div class="text-center w-11/12 mx-auto">
             <h3 class=" tracking-[.3rem]">BENEFICIOS</h3>
             <h2 class="font-medium my-3 text-4xl">¿Por qué Entrena en Casa?</h2>
             <p class="font-light">Entrena en Casa es la nueva forma de ejercitarse y vivir de forma más saludable.</p>
         </div>
         <div class="flex my-10 justify-between">
-            <img class="absolute" src="/home/woman-exercising.png" alt="woman exercising" width="500">
-            <div class="flex justify-end items-center w-11/12 mx-auto">
+            <img class="hidden xl:block absolute" src="/home/woman-exercising.png" alt="woman exercising" width="500">
+            <div class="flex justify-center xl:justify-end items-center w-11/12 mx-auto">
                 <ul class="text-start space-y-2 flex flex-col items-center">
                     <li class="flex w-3/4">
                         <img class="mr-2" src="/icons/checkmark-blue.svg" alt="" width="25">
