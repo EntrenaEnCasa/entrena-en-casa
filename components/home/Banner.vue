74472c2b87cda06c02da050886fc3321828684d5
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-08-04 13:34:05 -0400

    make home banner component responsive

--- components/home/Banner.vue
+++ components/home/Banner.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="pt-10 bg-gradiente relative" style="padding-bottom: 100px">
+    <div class="py-10 md:pb-32 bg-gradiente relative">
         <div class="tellural">
-            <h1 class="text-center font-bold text-5xl">
+            <h1 class="w-11/12 mx-auto text-center font-bold text-4xl md:text-5xl">
                 Son cada vez más
                 <br>
                 alumnos entrenando
                 <br>
-                <div class="mt-4 inline-block px-5 py-2 text-white rounded-xl text-3xl font-bold bg-primary">
+                <div class="mt-4 inline-block px-5 py-2 text-white rounded-xl text-2xl md:text-3xl font-bold bg-primary">
                     online
                 </div>
             </h1>
-            <div class="grid grid-cols-3 place-items-center">
-                <div class="flex flex-col space-y-4">
+            <div class="grid grid-cols-1 md:grid-cols-3 place-items-center mt-5 md:mt-0">
+                <div class="hidden md:flex flex-col space-y-4">
                     <div class="self-start ml-8">
                         <img src="/icons/star.svg" alt="running" width="60">
                     </div>
@@ -30,7 +30,7 @@
                         metodología
                     </div>
                 </div>
-                <div class="flex">
+                <div class="hidden md:flex">
                     <img src="/home/yoga-couple.png" alt="yoga-couple" width="200">
                     <div>
                         <div>
@@ -53,43 +53,51 @@
                 </div>
             </div>
         </div>
-
-        <div class="rounded-lg shadow-lg bg-white w-10/12 p-4 absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2">
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 place-items-center">
-                <div class="flex">
-                    <img src="/icons/location.svg" alt="location icon" width="40">
-                    <div class="ml-1 flex flex-col leading-tight">
-                        <p class="font-semibold mb-1">Modalidad
-                        </p>
+        <div
+            class="mt-12 mx-auto rounded-lg shadow-lg bg-white w-10/12 p-8 md:absolute md:bottom-0 md:left-1/2 md:-translate-x-1/2 md:translate-y-1/2 ">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                <div class="w-full flex items-center gap-2">
+                    <div>
+                        <Icon name="fa6-solid:location-dot" class="text-4xl w-16 min-w-16" />
+                    </div>
+                    <div class="flex flex-col leading-tight">
+                        <h4 class="font-semibold mb-1">
+                            Modalidad
+                        </h4>
                         <p class="text-sm">Online o presencial, tu escoges</p>
                     </div>
                 </div>
-                <div class="flex">
-                    <img src="/icons/dumbbell.svg" alt="location icon" width="40">
-                    <div class="ml-2 flex flex-col leading-tight">
+                <div class="w-full flex items-center gap-2">
+                    <div>
+                        <Icon name="fa6-solid:dumbbell" class="text-4xl w-16" />
+                    </div>
+
+                    <div class="flex flex-col leading-tight">
                         <p class="font-semibold mb-1">Modalidad
                         </p>
                         <p class="text-sm">Programa tus propias
                             sesiones, ve a tu propio ritmo</p>
                     </div>
                 </div>
-                <div class="flex">
-                    <img src="/icons/user.svg" alt="location icon" width="40">
-                    <div class="ml-1 flex flex-col leading-tight">
+                <div class="w-full flex items-center gap-2">
+                    <div>
+                        <Icon name="fa6-solid:user" class="text-4xl w-16" />
+                    </div>
+
+                    <div class="flex flex-col leading-tight">
                         <p class="font-semibold mb-1">Modalidad
                         </p>
                         <p class="text-sm">Entrena tanto
                             individual como grupal</p>
                     </div>
                 </div>
-                <div class="flex">
+                <div class="flex items-center justify-center md:justify-start">
                     <router-link to="/user/auth/register" class="border-0 rounded-lg text-white px-3 py-2 font-medium"
                         style="background-color: #00A4E8">Registrarse
                         ahora
                     </router-link>
                 </div>
             </div>
-
         </div>
     </div>
 </template>
