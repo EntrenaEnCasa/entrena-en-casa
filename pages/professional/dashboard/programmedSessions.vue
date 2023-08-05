6dc7261cfcdaad34c34d06f141ced383c199a98e
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-08-04 21:56:11 -0400

    add new programmed sessions component

---
+++ pages/professional/dashboard/programmedSessions.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="p-6 sm:p-8 space-y-6">
+        <div>
+            <h3 class="text-xl font-medium mb-5">Sesiones próximas</h3>
+            <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                <div class="px-6 py-3 rounded-md shadow border space-y-4">
+                    <div class="flex justify-between text-gray-400 text-sm">
+                        <p>25 de junio</p>
+                        <p>16:00hrs</p>
+                    </div>
+                    <div class="text-center space-y-2">
+                        <h4 class="font-medium text-xl">Tipo de sesión</h4>
+                        <a href="#" class="font-medium text-lg text-secondary underline">Lugar</a>
+                        <p class="text-gray-400">3 Participantes</p>
+                    </div>
+                    <div class="text-center">
+                        <button class="px-2 py-1 rounded-md bg-primary text-white font-medium">Ver detalles</button>
+                    </div>
+                </div>
+                <div class="px-6 py-3 rounded-md shadow border space-y-4">
+                    <div class="flex justify-between text-gray-400 text-sm">
+                        <p>25 de junio</p>
+                        <p>16:00hrs</p>
+                    </div>
+                    <div class="text-center space-y-2">
+                        <h4 class="font-medium text-xl">Tipo de sesión</h4>
+                        <a href="#" class="font-medium text-lg text-secondary underline">Online</a>
+                        <p class="text-gray-400">10 Participantes</p>
+                    </div>
+                    <div class="text-center">
+                        <button class="px-2 py-1 rounded-md bg-primary text-white font-medium">Ver detalles</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div>
+            <h3 class="text-xl font-medium mb-5">Sesiones pasadas</h3>
+            <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                <div class="px-6 py-3 rounded-md shadow border space-y-4 opacity-60">
+                    <div class="flex justify-between text-gray-400 text-sm">
+                        <p>25 de junio</p>
+                        <p>16:00hrs</p>
+                    </div>
+                    <div class="text-center space-y-2">
+                        <h4 class="font-medium text-xl">Tipo de sesión</h4>
+                        <a href="#" class="font-medium text-lg text-secondary underline">Lugar</a>
+                        <p class="text-gray-400">3 Participantes</p>
+                    </div>
+                    <div class="text-center">
+                        <button class="px-2 py-1 rounded-md bg-primary text-white font-medium">Ver detalles</button>
+                    </div>
+                </div>
+                <div class="px-6 py-3 rounded-md shadow border space-y-4 opacity-60">
+                    <div class="flex justify-between text-gray-400 text-sm">
+                        <p>25 de junio</p>
+                        <p>16:00hrs</p>
+                    </div>
+                    <div class="text-center space-y-2">
+                        <h4 class="font-medium text-xl">Tipo de sesión</h4>
+                        <a href="#" class="font-medium text-lg text-secondary underline">Online</a>
+                        <p class="text-gray-400">10 Participantes</p>
+                    </div>
+                    <div class="text-center">
+                        <button class="px-2 py-1 rounded-md bg-primary text-white font-medium">Ver detalles</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script setup>
+</script>
