f46d54448b54f6f3fdda9256c2407a38c41912e8
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-08-06 18:46:35 -0400

    update schedule component to use takenTimes logic

--- pages/professional/dashboard/schedule.vue
+++ pages/professional/dashboard/schedule.vue
@@ -1,7 +1,14 @@
 <template>
     <div class="p-6 sm:p-8">
-        <div class="flex items-center justify-between mb-4">
+        <div class="flex flex-col sm:flex-row items-center justify-between mb-4">
             <h3 class="text-xl font-medium">Horario disponible</h3>
+            <div class="flex justify-center items-center gap-3">
+                <span>
+                    Disponible
+                </span>
+                <div class="w-14 h-10 bg-primary rounded-md">
+                </div>
+            </div>
         </div>
         <div class="overflow-x-auto sm:rounded-lg">
             <table class="bg-white w-full table-fixed text-sm text-gray-500 border">
@@ -16,22 +23,18 @@
                 </thead>
                 <tbody>
                     <tr v-for="time, index in timesList" :key="index">
-                        <td class="py-4 pr-6 text-right font-semibold whitespace-nowrap">
-                            {{ time }}
+                        <td class="h-14 pr-6 text-right font-semibold whitespace-nowrap">
+                            {{ formatTime(time) }}
                         </td>
-                        <td class="border">
-                        </td>
-                        <td class="border">
-                        </td>
-                        <td class="border">
-                        </td>
-                        <td class="border">
-                        </td>
-                        <td class="border">
-                        </td>
-                        <td class="border">
-                        </td>
-                        <td class="border">
+                        <td v-for="n in 7" :key="n" class="h-14 border">
+                            <div v-if="!timeTaken(n, time)"
+                                class="w-full h-full flex items-center justify-center group cursor-pointer">
+                                <div class="hidden group-hover:flex">
+                                    <Icon name="fa6-solid:square-plus" class="text-3xl text-gray-300" />
+                                </div>
+                            </div>
+                            <div v-else class="w-full h-full bg-primary">
+                            </div>
                         </td>
                     </tr>
                 </tbody>
@@ -46,6 +49,20 @@ import { ref, onMounted } from "vue";
 
 const daysList = ref([]);
 const timesList = ref([]);
+const takenTimes = [
+    {
+        date: new Date(2023, 8, 6, 9),
+    },
+    {
+        date: new Date(2023, 8, 8, 10),
+    },
+    {
+        date: new Date(2023, 8, 8, 13),
+    },
+    {
+        date: new Date(2023, 8, 12, 18),
+    }
+];
 
 const generateDaysList = () => {
     const today = new Date();
@@ -68,8 +85,7 @@ const generateTimesList = () => {
     const endTime = 20; // 8 PM
 
     for (let i = startTime; i <= endTime; i++) {
-        const formattedTime = formatTime(i);
-        timesList.value.push(formattedTime);
+        timesList.value.push(i);
     }
 }
 
@@ -78,6 +94,15 @@ const formatTime = (hour) => {
     const formattedHour = hour % 12 === 0 ? 12 : hour % 12;
 
     return `${formattedHour} ${amPm}`;
+}
+
+const timeTaken = (n, time) => {
+    const date = new Date(daysList.value[n - 1]);
+    date.setHours(time);
+
+    return takenTimes.some((takenTime) => {
+        return (takenTime.date.getHours() === date.getHours() && takenTime.date.getDate() === date.getDate());
+    });
 }
 
 onMounted(() => {
