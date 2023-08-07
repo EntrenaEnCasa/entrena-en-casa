a28e9a0e53ec71d8ba950cbcf87474a6a629ef30
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-08-07 14:34:44 -0400

    add login functionality

--- pages/user/auth/login.vue
+++ pages/user/auth/login.vue
@@ -14,7 +14,7 @@
             <nuxt-link to="/">
                 <img src="/logo.png" class="mb-4 w-36" alt="logo">
             </nuxt-link>
-            <form class="w-4/5 space-y-5" @submit.prevent="submit">
+            <form class="w-4/5 space-y-5" @submit.prevent="login">
                 <div>
                     <label class="block mb-1" for="email">Correo electrónico</label>
                     <div class="border rounded-md px-4 py-3 flex items-center space-x-4 w-full"
@@ -22,7 +22,7 @@
                         <div>
                             <Icon name="fa6-solid:envelope" class="text-secondary text-lg" />
                         </div>
-                        <input class="w-full" type="email" id="email" aria-describedby="emailHelp"
+                        <input v-model="formData.email" class="w-full" type="email" id="email" aria-describedby="emailHelp"
                             placeholder="Ingresa tu correo electrónico">
                     </div>
                 </div>
@@ -33,7 +33,8 @@
                         <div>
                             <Icon name="fa6-solid:lock" class="text-secondary text-lg" />
                         </div>
-                        <input type="password" class="w-full" id="password" placeholder="* * * * * * * *">
+                        <input v-model="formData.password" type="password" class="w-full" id="password"
+                            placeholder="* * * * * * * *">
                     </div>
 
                 </div>
@@ -44,11 +45,8 @@
                     </div>
                     <p class=" text-secondary">Olvidé mi contraseña</p>
                 </div>
-                <nuxt-link class="block" to="/user/dashboard/home">
-                    <button type="submit"
-                        class="bg-primary py-3 w-full text-white font-medium mt-2 text-xl rounded-sm">Iniciar
-                        sesión</button>
-                </nuxt-link>
+                <button type="submit" class="bg-primary py-3 w-full text-white font-medium mt-2 text-xl rounded-sm">Iniciar
+                    sesión</button>
             </form>
             <p class="mt-4 text-gray-500">
                 ¿No tienes cuenta?
@@ -61,36 +59,69 @@
 </template>
 <script setup>
 
-//    import {useUserStore} from '~/stores/userStore'
+import { useUserStore } from '~/stores/UserStore'
+import { useAuthStore } from '~/stores/AuthStore'
 
-//     const router = useRouter();
-//     const userStore = useUserStore();
-
-//     const formData = reactive({
-//         email: "",
-//         password: ""
-//     });
+const router = useRouter();
+const userStore = useUserStore();
+const authStore = useAuthStore();
+const formData = reactive({
+    email: "",
+    password: ""
+});
 
 definePageMeta({
     layout: "auth",
 });
 
-const login = () => {
-    console.log("login");
+const login = async () => {
+
+    const { data, pending, error, refresh } = await useFetch('http://localhost:3002/login', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: {
+            correo: formData.email,
+            contraseña: formData.password,
+        },
+        onResponse({ request, response, options }) {
+
+            const responseData = response._data;
+
+            // Process the response data
+            if (responseData.success) {
+                getCredits(responseData);
+            }
+            else {
+                alert(responseData.error);
+            }
+        },
+    });
 }
 
-//     const login = () => {
-//         if(formData.email == "" || formData.password == ""){
-//             alert("Por favor, ingrese todos los datos");
-//             return;
-//         }
-//         //llamada a la api
+const getCredits = async (responseData) => {
+    const { data, pending, error, refresh } = useFetch("http://localhost:3002/obtenercreditos/", {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: {
+            idUsuario: responseData.id_usuario
+        },
+        onResponse({ request, response, options }) {
+            const responseDataCredits = response._data;
+            if (responseDataCredits.success) {
+                authStore.logIn(responseData, responseDataCredits.creditos);
+                router.push('/user/dashboard/home');
+            }
+            else {
+                alert(responseData.error);
+            }
+        },
+    });
 
-//         //guardar en el store
-//         userStore.setName();
-//         //una vez que te logeas, te envía a la página de usuario
-//         router.push('/user/dashboard/home');
-//     }
+}
 
 </script>
 <style scoped lang="scss">
