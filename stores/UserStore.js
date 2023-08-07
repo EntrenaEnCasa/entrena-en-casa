146d354b6c35ba0205e741825876fb96247e1300
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-08-07 14:12:09 -0400

    add UserStore

--- stores/UserStore.js
+++ stores/UserStore.js
@@ -3,16 +3,35 @@ import { defineStore } from 'pinia'
 export const useUserStore = defineStore('UserStore', {
     state: () => {
         return {
-            user: {},
-            loggedIn: false
+            user: null || {
+                id: null,
+                email: null,
+                role: null,
+                credits: {
+                    bronze: null,
+                    silver: null,
+                    gold: null
+                }
+            },
         }
     },
     actions: {
-        setUser(user) {
+        setUserData(user) {
             this.user = user;
+            this.storeUserInLocalStorage(user);
         },
-        setLoggedIn(loggedIn) {
-            this.loggedIn = loggedIn;
+        removeUserData() {
+            this.user = null;
+            this.removeUserFromLocalStorage();
+        },
+        storeUserInLocalStorage(user) {
+            localStorage.setItem('user', JSON.stringify(user));
+        },
+        setUserFromLocalStorage() {
+            this.user = JSON.parse(localStorage.getItem('user'));
+        },
+        removeUserFromLocalStorage() {
+            localStorage.removeItem('user');
         }
     }
 })
