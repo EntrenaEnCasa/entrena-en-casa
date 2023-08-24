856ec5790c7b119c71889fd5f84506b3d9319e07
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-08-24 16:39:19 -0400

    add new Button component

---
+++ components/common/Button.vue
@@ -0,0 +1,35 @@
+<template>
+    <button class="bg-primary text-white rounded-sm" :class="'text-' + size">
+        <span v-if="!loading">
+            {{ text }}
+        </span>
+        <div v-else>
+            <span>{{ loadingText }}</span>
+            <span>
+                <Icon :class="'text-' + size" class="animate-spin text-2xl text-white ml-2" name="fa6-solid:circle-notch" />
+            </span>
+        </div>
+    </button>
+</template>
+<script setup>
+
+const props = defineProps({
+    text: {
+        type: String,
+        default: 'Botón'
+    },
+    loading: {
+        type: Boolean,
+        default: false
+    },
+    loadingText: {
+        type: String,
+        default: 'Cargando'
+    },
+    size: {
+        type: String,
+        default: 'base'
+    }
+});
+
+</script>
