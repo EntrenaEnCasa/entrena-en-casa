6249eb678a4f3ad170b272d5c4ea47bb6f637c38
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-08-23 20:55:53 -0400

    use api to get sessions for students

--- pages/user/dashboard/sessions.vue
+++ pages/user/dashboard/sessions.vue
@@ -2,9 +2,13 @@
     <div>
         <div class="space-y-4">
             <div>
-                <h3 class="text-xl font-medium">Sesión en curso</h3>
+                <h3 class="text-xl font-medium">Sesiones en curso</h3>
             </div>
-            <div>
+            <div class="bg-white py-4 px-6 rounded-2xl border border-zinc-200 gap-6 items-center space-y-3"
+                style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
+                <div class="text-md text-center"><b>No hay sesiones registradas</b></div>
+            </div>
+            <!-- <div>
                 <div class="bg-white lg:h-15 py-4 px-6 rounded-2xl border border-zinc-200 lg:flex gap-6 items-center justify-evenly"
                     style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
                     <div class="text-[#949494] text-sm">16:00hrs</div>
@@ -22,13 +26,17 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> -->
             <div>
                 <h3 class="text-xl font-medium">Sesiones próximas</h3>
             </div>
-            <div v-if="sessions.length > 0">
+            <div v-if="futureSessions.loading">
+                Cargando datos...
+            </div>
+            <div v-else-if="futureSessions.success">
                 <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
-                    <div class="bg-white py-4 px-6 rounded-2xl border border-zinc-200 gap-6 items-center space-y-3"
+                    <!-- <div
+                        class="bg-white py-4 px-6 rounded-2xl border border-zinc-200 gap-6 items-center space-y-3"
                         style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
                         <div class="px-3">
                             <div class="grid grid-cols-2 gap-1">
@@ -43,21 +51,32 @@
                             <div class="text-[#949494] text-base text-center">Nombre Profesional - Tipo Profesional</div>
                             <div class="text-[#000000] text-2xl font-medium text-center">Lugar</div>
                         </div>
-                    </div>
-                    <div class="bg-white py-4 px-6 rounded-2xl border border-zinc-200 gap-6 items-center space-y-5"
+                    </div> -->
+                    <div v-for="session in futureSessions.sessions" :key="session.session_id"
+                        class="bg-white py-4 px-6 rounded-2xl border border-zinc-200 gap-6 items-center space-y-5"
                         style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
                         <div class="px-3">
                             <div class="grid grid-cols-2 gap-1">
-                                <div class="text-[#949494] text-sm text-left">
-                                    <p>26 de Julio</p>
+                                <div class="text-[#949494] text-sm">
+                                    <p>{{ session.date }}</p>
                                 </div>
                                 <div class="text-[#949494] text-sm text-right">
-                                    <p>16:00hrs</p>
+                                    <p>{{ session.time }}hrs</p>
                                 </div>
                             </div>
-                            <div class="text-[#000000] text-lg font-medium text-center"><b>Tipo de sesión</b></div>
-                            <div class="text-[#949494] text-base text-center">Nombre Profesional - Tipo Profesional</div>
-                            <div class="text-[#000000] text-2xl font-medium text-center">Lugar</div>
+                            <div class="flex flex-col items-center gap-y-2">
+                                <b class="text-[#000000] text-lg font-medium ">{{ session.format + ' - ' +
+                                    session.modality }}</b>
+                                <a :href="session.link" target="_blank"
+                                    class="text-xl font-medium underline text-secondary decoration-secondary underline-offset-2">Link</a>
+                                <p class="text-[#949494]">{{ session.professional.first_name + ' ' +
+                                    session.professional.last_name + ' - ' + session.professional.title }}</p>
+                            </div>
+                            <div class=" text-center"></div>
+                            <div class=" text-center"></div>
+                            <div>
+
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -65,29 +84,38 @@
             <div v-else>
                 <div class="bg-white py-4 px-6 rounded-2xl border border-zinc-200 gap-6 items-center space-y-3"
                     style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
-                    <div class="text-md  text-center"><b>No hay sesiones disponibles</b></div>
+                    <div class="text-md  text-center"><b>{{ futureSessions.message }}</b></div>
                 </div>
             </div>
 
-            <!-- <div>
+            <div>
                 <h3 class="text-xl font-medium">Sesiones pasadas</h3>
             </div>
-            <div>
+            <div v-if="pastSessions.loading">
+                Cargando datos...
+            </div>
+            <div v-else-if="pastSessions.success">
                 <div class="grid grid-cols-1 lg:grid-cols-2 gap-6">
-                    <div class="bg-white py-4 px-6 rounded-2xl border border-zinc-200 gap-6 items-center space-y-3 opacity-60"
+                    <div v-for="session in pastSessions.sessions" :key="session.session_id"
+                        class="bg-white py-4 px-6 rounded-2xl border border-zinc-200 gap-6 items-center space-y-3 opacity-60"
                         style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
                         <div class="px-3">
                             <div class="grid grid-cols-2 gap-1">
                                 <div class="text-[#949494] text-sm text-left">
-                                    <p>25 de Junio</p>
+                                    <p>{{ session.date }}</p>
                                 </div>
                                 <div class="text-[#949494] text-sm text-right">
-                                    <p>16:00hrs</p>
+                                    <p>{{ session.time }}hrs</p>
                                 </div>
                             </div>
-                            <div class="text-[#000000] text-lg font-medium text-center"><b>Tipo de sesión</b></div>
-                            <div class="text-[#949494] text-base text-center">Nombre Profesional - Tipo Profesional</div>
-                            <div class="text-[#0EB3E0] text-2xl font-medium text-center"><a href="#">Online</a></div>
+                            <div class="flex flex-col items-center gap-y-2">
+                                <b class="text-[#000000] text-lg font-medium ">{{ session.format + ' - ' +
+                                    session.modality }}</b>
+                                <a :href="session.link" target="_blank"
+                                    class="text-xl font-medium underline text-secondary decoration-secondary underline-offset-2">Link</a>
+                                <p class="text-[#949494]">{{ session.professional.first_name + ' ' +
+                                    session.professional.last_name + ' - ' + session.professional.title }}</p>
+                            </div>
                             <div class="text-[#949494] text-base text-center gap-2">
                                 <p>Calificar sesión</p>
                                 <div class="flex place-content-center">
@@ -100,7 +128,7 @@
                             </div>
                         </div>
                     </div>
-                    <div class="bg-white py-4 px-6 rounded-2xl border border-zinc-200 gap-6 items-center space-y-3 opacity-60"
+                    <!-- <div class="bg-white py-4 px-6 rounded-2xl border border-zinc-200 gap-6 items-center space-y-3 opacity-60"
                         style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
                         <div class="px-3">
                             <div class="grid grid-cols-2 gap-1">
@@ -125,36 +153,75 @@
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </div> -->
                 </div>
-
-        </div> -->
+            </div>
+            <div v-else>
+                <div class="bg-white py-4 px-6 rounded-2xl border border-zinc-200 gap-6 items-center space-y-3"
+                    style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
+                    <div class="text-md  text-center"><b>{{ pastSessions.message }}</b></div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <script setup>
 import { ref, onMounted } from "vue";
 import { useUserStore } from '~/stores/UserStore'
-const userStore = useUserStore();
-const sessions = ref([]);
 
-onMounted(async () => {
-    await getScheduledSessions();
+const userStore = useUserStore();
+const runtimeConfig = useRuntimeConfig();
+
+const loadingData = ref(false);
+
+const futureSessions = ref({
+    success: false,
+    loading: false,
+    message: '',
+    sessions: [],
 });
 
-const getScheduledSessions = async () => {
-    const { data, pending, error, refresh } = await useFetch('http://localhost:3002/informacion-sesiones/:idUsuario', {
+const pastSessions = ref({
+    success: false,
+    loading: false,
+    message: '',
+    sessions: [],
+});
+
+onMounted(async () => {
+    await getFutureSessions();
+    await getPastSessions();
+});
+
+const getFutureSessions = async () => {
+
+    loadingData.value = true;
+
+    await useFetch(`${runtimeConfig.public.apiBase}/student/${userStore.user.user_id}/sessions/future`, {
         method: 'GET',
         headers: {
             "Content-Type": "application/json",
-        },
-        body: {
-            idUsuario: userStore.user.idUsuario,
+            "x-access-token": userStore.getUserToken(),
         },
         onResponse({ request, response, options }) {
-            if (response._data.success) {
-                sessions.value = response._data.sesiones;
-            }
+            futureSessions.value = response._data;
+            futureSessions.loading = false;
+        },
+    });
+}
+
+const getPastSessions = async () => {
+    loadingData.value = true;
+
+    await useFetch(`${runtimeConfig.public.apiBase}/student/${userStore.user.user_id}/sessions/past`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+            "x-access-token": userStore.getUserToken(),
+        },
+        onResponse({ request, response, options }) {
+            pastSessions.value = response._data;
+            pastSessions.loading = false;
         },
     });
 }
