5ff6c0e0cad10f2254ded7e78a26a57dfd08521e
Gonzalo Bruna <catalina.schencke.p@mail.pucv.cl>
2023-10-30 17:48:07 -0300

    fix modal  component not having min width

--- components/common/Modal.vue
+++ components/common/Modal.vue
@@ -3,7 +3,7 @@
         <div v-if="isOpen"
             class="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-screen max-h-full flex justify-center items-center backdrop-blur-sm bg-black/30"
             :class="{ 'modal-open': isOpen }" @click="closeModal">
-            <div class="w-full max-w-4xl max-h-full relative shadow rounded-2xl" @click.stop>
+            <div class="min-w-3xl max-h-full relative shadow rounded-2xl" @click.stop>
                 <div class="w-full h-full bg-white p-4 flex flex-col justify-between">
                     <div>
                         <button type="button" class="bg-transparent hover:bg-gray-100 transition duration-75 rounded-lg p-1"
