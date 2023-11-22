9c0aa5fce9f3733ce10c1181ba57aca272fd4c7f
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-11-22 19:18:07 -0300

    Change button from "agendar" to "comprar" in credits

--- pages/user/dashboard/credits.vue
+++ pages/user/dashboard/credits.vue
@@ -67,7 +67,7 @@
                         <td class=" py-4 ">
                             <button class="px-4 py-2 bg-primary text-white rounded-md font-medium"
                                 @click="addCredits(item)">
-                                Agendar
+                                Comprar
                             </button>
                         </td>
                     </tr>
