6897560eab88bd82c6a9ebe8e76c1a96babea2d4
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2023-11-22 18:43:56 -0300

    use real data for sessions in home

--- pages/user/dashboard/home.vue
+++ pages/user/dashboard/home.vue
@@ -1,15 +1,7 @@
 <template >
     <div class="space-y-4">
         <div class="grid grid-cols-1 lg:grid-cols-2 gap-4">
-            <div class="bg-white py-6 px-10 rounded-2xl border border-zinc-200"
-                style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
-                <div>
-                    <h5 class="text-lg text-[#949494]">Últimas sesiones</h5>
-                </div>
-                <div class="mt-5">
-                    <p class="text-lg text-[#949494]">No hay estadísticas disponibles actualmente.</p>
-                </div>
-            </div>
+
             <!-- <div class="grid grid-rows-2"> -->
             <div class="bg-white py-6 px-10 rounded-2xl border border-zinc-200"
                 style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
@@ -17,76 +9,57 @@
                     <h5 class="text-lg text-[#949494]">Últimas sesiones</h5>
                     <p class="text-right text-sm font-medium text-gray-800">Ver todas</p>
                 </div>
-                <div class="grid grid-cols-3">
-                    <div>16:00hrs</div>
-                    <div>Tipo de sesión</div>
-                    <button class="text-secondary font-semibold place-self-end flex items-center">
-                        <p class="hidden md:block mr-1">Ver más</p>
-                        <Icon name="fa6-solid:chevron-right" />
-                    </button>
+                <CommonLoading v-if="futureSessions.loading" />
+                <div v-else-if="futureSessions.success">
+                    <div v-for="session in futureSessions.sessions" :key="session.session_id">
+                        <hr class="my-5">
+                        <div class="grid grid-cols-3 items-center">
+                            <div>{{ session.time }}hrs</div>
+                            <div class="text-sm"> {{ session.format + ' - ' + session.modality }} </div>
+                            <button class="text-secondary font-semibold place-self-end flex items-center">
+                                <p class="hidden md:block mr-1">Ver más</p>
+                                <Icon name="fa6-solid:chevron-right" />
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <hr class="my-5">
-                <div class="grid grid-cols-3">
-                    <div>16:00hrs</div>
-                    <div>Tipo de sesión</div>
-                    <button class="text-secondary font-semibold place-self-end flex items-center">
-                        <p class="hidden md:block mr-1">Ver más</p>
-                        <Icon name="fa6-solid:chevron-right" />
-                    </button>
+                <div v-else>
+                    <div class="bg-white py-4 px-6 rounded-2xl border border-zinc-200 gap-6 items-center space-y-3"
+                        style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
+                        <div class="text-md  text-center"><b>No tienes ninguna sesión a futuro.</b></div>
+                    </div>
                 </div>
-                <hr class="my-5">
-                <div class="grid grid-cols-3">
-                    <div>16:00hrs</div>
-                    <div>Tipo de sesión</div>
-                    <button class="text-secondary font-semibold place-self-end flex items-center">
-                        <p class="hidden md:block mr-1">Ver más</p>
-                        <Icon name="fa6-solid:chevron-right" />
-                    </button>
-                </div>
+
             </div>
             <!-- </div> -->
-            <div class="bg-white h-72 py-4 px-6 rounded-2xl border border-zinc-200"
-                style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
-                <div>
-                    <h5 class="text-lg text-[#949494]">Próximas sesiones</h5>
-                </div>
-                <div class="mt-5">
-                    <p class="text-lg text-[#949494]">No hay estadísticas disponibles actualmente.</p>
-                </div>
-            </div>
             <div class="bg-white py-6 px-10 rounded-2xl border border-zinc-200"
                 style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
                 <div class="mb-5 flex justify-between items-center">
                     <h5 class="text-lg text-[#949494]">Próximas sesiones</h5>
                     <p class="text-right text-sm font-medium text-gray-800">Ver todas</p>
                 </div>
-                <div class="grid grid-cols-3">
-                    <div>16:00hrs</div>
-                    <div>Tipo de sesión</div>
-                    <button class="text-secondary font-semibold place-self-end flex items-center">
-                        <p class="hidden md:block mr-1">Ver más</p>
-                        <Icon name="fa6-solid:chevron-right" />
-                    </button>
+                <CommonLoading v-if="pastSessions.loading" />
+                <div v-else-if="pastSessions.success">
+                    <div v-for="session in pastSessions.sessions" :key="session.session_id">
+                        <hr class="my-5">
+                        <div class="grid grid-cols-3 items-center">
+                            <div>{{ session.time }}hrs</div>
+                            <div class="text-sm"> {{ session.format + ' - ' + session.modality }} </div>
+                            <button class="text-secondary font-semibold place-self-end flex items-center">
+                                <p class="hidden md:block mr-1">Ver más</p>
+                                <Icon name="fa6-solid:chevron-right" />
+                            </button>
+                        </div>
+                    </div>
                 </div>
-                <hr class="my-5">
-                <div class="grid grid-cols-3">
-                    <div>16:00hrs</div>
-                    <div>Tipo de sesión</div>
-                    <button class="text-secondary font-semibold place-self-end flex items-center">
-                        <p class="hidden md:block mr-1">Ver más</p>
-                        <Icon name="fa6-solid:chevron-right" />
-                    </button>
-                </div>
-                <hr class="my-5">
-                <div class="grid grid-cols-3">
-                    <div>16:00hrs</div>
-                    <div>Tipo de sesión</div>
-                    <button class="text-secondary font-semibold place-self-end flex items-center">
-                        <p class="hidden md:block mr-1">Ver más</p>
-                        <Icon name="fa6-solid:chevron-right" />
-                    </button>
+                <div v-else>
+                    <div class="bg-white py-4 px-6 rounded-2xl border border-zinc-200 gap-6 items-center space-y-3"
+                        style="box-shadow: 0px 4px 50px -16px rgba(0, 0, 0, 0.10);">
+                        <div class="text-md  text-center"><b>No has participado de ninguna sesión.</b></div>
+                    </div>
                 </div>
             </div>
+
         </div>
         <div>
             <div class="bg-white h-72 py-4 px-6 rounded-2xl border border-zinc-200"
@@ -108,3 +81,64 @@
     box-shadow: 0px -29px 89px 0px rgba(0, 0, 0, 0.10);
 }
 </style>
+
+<script setup>
+import { reactive, onMounted } from "vue";
+import { useUserStore } from '~/stores/UserStore'
+
+const userStore = useUserStore();
+const runtimeConfig = useRuntimeConfig();
+
+const futureSessions = ref({
+    success: false,
+    loading: false,
+    message: '',
+    sessions: [],
+});
+
+const pastSessions = ref({
+    success: false,
+    loading: false,
+    message: '',
+    sessions: [],
+});
+
+onMounted(async () => {
+    await getFutureSessions();
+    await getPastSessions();
+});
+
+const getFutureSessions = async () => {
+
+    futureSessions.value.loading = true;
+
+    await useFetch(`${runtimeConfig.public.apiBase}/student/${userStore.user.user_id}/sessions/soon`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+            "x-access-token": userStore.getUserToken(),
+        },
+        onResponse({ request, response, options }) {
+            futureSessions.value = response._data;
+            futureSessions.value.loading = false;
+        },
+    });
+}
+
+const getPastSessions = async () => {
+
+    pastSessions.value.loading = true;
+
+    await useFetch(`${runtimeConfig.public.apiBase}/student/${userStore.user.user_id}/sessions/last`, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json",
+            "x-access-token": userStore.getUserToken(),
+        },
+        onResponse({ request, response, options }) {
+            pastSessions.value = response._data;
+            pastSessions.value.loading = false;
+        },
+    });
+}
+</script>
