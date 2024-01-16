5f7375552764b41ae62ede653b1ad622f16dfa3d
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-01-16 15:37:10 -0300

    add new ClientSearchInput component

---
+++ components/professional/dashboard/calendar/ClientSearchInput.vue
@@ -0,0 +1,131 @@
+<template>
+    <div class="relative">
+        <div class="border text-gray-800 text-sm rounded-md px-5 py-3.5 max-w-3xl box-border"
+            :class="{ 'ring-2 ring-primary ring-inset': inputFocused }">
+            <div class="flex items-center flex-wrap gap-y-4 gap-x-2">
+                <input type="text" v-model="searchTerm" :placeholder="placeholder" class="outline-none w-full"
+                    @focus="inputFocused = true" @blur="inputFocused = false" @input="delayedFetchResults">
+                <div class="" v-for="(chip, index) in chips" :key="index">
+                    <span
+                        class="inline-flex items-center px-2.5 py-1.5 rounded-full text-xs font-medium bg-secondary text-white">
+                        <Icon @click="removeChip(index)" class="text-lg mr-1" name="fa6-solid:circle-xmark" />
+                        <span>
+                            {{ chip }}
+                        </span>
+                    </span>
+                </div>
+            </div>
+            <div class="border rounded-md absolute top-[110%] left-0 right-0 bg-white shadow-lg p-3"
+                v-if="inputFocused && searchTerm">
+                <CommonLoading v-if="isLoading" text="Buscando" />
+                <ul v-else>
+                    <li v-show="(isSearchPending || isLoading) && filteredResults.length === 0" class="px-3 py-2">
+                        Se buscará {{ searchTerm }}
+                    </li>
+                    <li class="px-3 py-2"
+                        v-if="filteredResults.length === 0 && !isLoading && hasFetched && !isSearchPending">
+                        No se encontraron resultados
+                    </li>
+                    <li class="px-3 py-2 rounded hover:bg-gray-100" v-show="filteredResults.length > 0"
+                        v-for="result in filteredResults" :key="result.user_id" @mousedown="addChip(result)">
+                        <p class="font-medium">
+                            {{ result.first_name || result.email }}
+                        </p>
+                        <p class="text-xs">
+                            {{ result.email }}
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref, computed, watch } from 'vue';
+import { useUserStore } from '~/stores/UserStore';
+
+const userStore = useUserStore();
+const runtimeConfig = useRuntimeConfig();
+
+const placeholder = 'Ingresa el correo electrónico o nombre';
+const searchTerm = ref('');
+const chips = ref([]);
+const inputFocused = ref(false);
+const results = ref([]);
+const isLoading = ref(false);
+const hasFetched = ref(false);
+const isSearchPending = ref(false);
+let timeoutId = null;
+
+const fetchResults = async () => {
+    if (searchTerm.value) {
+        isSearchPending.value = false; // Reset here
+        isLoading.value = true;
+        hasFetched.value = false;
+        results.value = [];
+        const { data, error } = await useFetch(`${runtimeConfig.public.apiBase}/professional/students`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                "x-access-token": userStore.getUserToken()
+            },
+            body: {
+                searchTerm: searchTerm.value
+            }
+        });
+
+        isLoading.value = false;
+        hasFetched.value = true;
+
+        if (error.value) {
+            console.log("Fetch error:", error.value);
+            return;
+        }
+
+        if (data.value.success) {
+            results.value = data.value.students;
+        }
+    }
+};
+
+const delayedFetchResults = () => {
+    clearTimeout(timeoutId);
+    isSearchPending.value = true; // Set this when a new search is initiated
+    timeoutId = setTimeout(() => {
+        if (searchTerm.value.trim() !== '') {
+            fetchResults();
+        } else {
+            results.value = [];
+            hasFetched.value = false;
+            isSearchPending.value = false; // Also reset here if search term is empty
+        }
+    }, 500);
+};
+
+watch(searchTerm, delayedFetchResults);
+
+const filteredResults = computed(() => {
+    if (!searchTerm.value && chips.value.length === 0) return results.value.slice(0, 5);
+
+    if (!searchTerm.value && chips.value.length > 0) return results.value.filter(student =>
+        !chips.value.includes(student.first_name || student.email)
+    );
+
+    return results.value.filter(student =>
+        ((student.first_name && student.first_name.toLowerCase().includes(searchTerm.value.toLowerCase())) ||
+            student.email.toLowerCase().includes(searchTerm.value.toLowerCase())) &&
+        !chips.value.includes(student.first_name || student.email)
+    ).slice(0, 5);
+});
+
+const addChip = (student) => {
+    chips.value.unshift(student.first_name || student.email);
+    searchTerm.value = '';
+};
+
+const removeChip = (index) => {
+    chips.value.splice(index, 1);
+};
+
+</script>
