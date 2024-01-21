80f4b8bf8d9d22e43fccf279ec04f2ba77c41034
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-01-21 01:50:23 -0300

    add Nuxt Mapbox module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: [
-    '@pinia/nuxt', '@nuxtjs/tailwindcss', 'nuxt-icon', '@vee-validate/nuxt'
+    '@pinia/nuxt', '@nuxtjs/tailwindcss', 'nuxt-icon', '@vee-validate/nuxt', 'nuxt-mapbox'
   ],
   runtimeConfig: {
     public: {
       apiBase: 'https://entrena-en-casa-backend-production.up.railway.app'
     }
-  }
+  },
+  mapbox: {
+    accessToken: 'pk.eyJ1IjoiZ29uemFsby1icnVuYSIsImEiOiJjbHJqcGlkcDgwMWZiMmtwOWliMHJsOGkxIn0.waguLODGXsYqrv8Ol0lwoQ',
+  },
 })
