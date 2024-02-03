6ab364e69fd638a59ecc51a21489d6e32ba0e932
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-02-03 15:09:23 -0300

    add new Session and Professional interfaces

---
+++ types/index.ts
@@ -0,0 +1,26 @@
+export {};
+
+declare global {
+
+  interface Professional {
+    user_id: number;
+    first_name: string;
+    last_name: string;
+    title: string;
+    phone: string | null;
+  }
+  
+  interface Session {
+    session_id: number;
+    date: string;
+    time: string;
+    available: number;
+    format: "Individual" | "Grupal";
+    modality: "Online" | "Presencial";
+    link: string | null;
+    credit_type: "bronze" | "silver" | "gold";
+    actual_assistant: number;
+    professional: Professional;
+  }
+  
+}
