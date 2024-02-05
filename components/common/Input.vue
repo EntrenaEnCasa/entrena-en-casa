c9afc2405c9932ca67af9f4b60548cfee9ae07a3
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-02-05 16:45:53 -0300

    fix commonInput v-if condition

--- components/common/Input.vue
+++ components/common/Input.vue
@@ -3,8 +3,8 @@
         <label v-if="label" class="block mb-1" :for="id">{{ label }}</label>
         <div class="border rounded-md px-4 py-3 flex items-center space-x-4 w-full"
             style="box-shadow: 0px 0px 27px -6px rgba(0, 0, 0, 0.10);">
-            <div>
-                <Icon v-if="icon" :name="icon" class="text-secondary text-lg" />
+            <div v-if="icon">
+                <Icon :name="icon" class="text-secondary text-lg" />
             </div>
             <Field :name="name" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"
                 class="w-full outline-none" :type="type" :id="id" :placeholder="placeholder" :rules="rules" />
