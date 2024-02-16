b18217ab35d4f2ef63962075b106108fe0f8c4b7
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-02-15 22:58:23 -0300

    remove not used code from user home component

--- pages/user/dashboard/home.vue
+++ pages/user/dashboard/home.vue
@@ -69,27 +69,13 @@
     </div>
 </template>
 
-<style scoped lang="scss">
-.userInicio {
-    background: linear-gradient(135deg, rgba(153, 208, 223, 0.10) 0%, rgba(255, 255, 255, 0.10) 0.01%, rgba(63, 136, 166, 0.10) 100%);
-    box-shadow: 0px -29px 89px 0px rgba(0, 0, 0, 0.10);
-}
-</style>
-
 <script setup>
-import { reactive, ref } from 'vue';
 import { useUserStore } from '~/stores/UserStore';
 
 const userStore = useUserStore();
 const runtimeConfig = useRuntimeConfig();
 
 const userId = ref(userStore.user.user_id); // Make user ID reactive
-
-// Reactive object to manage loading states
-const loadingStates = reactive({
-    futureSessions: false,
-    pastSessions: false,
-});
 
 // Fetch future sessions
 const { data: futureSessions, pending: futureSessionsLoading, refresh: refreshFutureSessions } = useFetch(
@@ -99,12 +85,6 @@ const { data: futureSessions, pending: futureSessionsLoading, refresh: refreshFu
         headers: {
             "Content-Type": "application/json",
             "x-access-token": userStore.userToken || '',
-        },
-        onResponse() {
-            loadingStates.futureSessions = false;
-        },
-        onFetchError() {
-            loadingStates.futureSessions = false;
         },
     }
 );
@@ -117,12 +97,6 @@ const { data: pastSessions, pending: pastSessionsLoading, refresh: refreshPastSe
         headers: {
             "Content-Type": "application/json",
             "x-access-token": userStore.userToken || '',
-        },
-        onResponse() {
-            loadingStates.pastSessions = false;
-        },
-        onFetchError() {
-            loadingStates.pastSessions = false;
         },
     }
 );
