607da75d742f8e1fa113ff022832dbc1c96b17d0
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-03-03 13:45:56 -0300

    add new log-in and log-out methods in the server

---
+++ server/api/auth/student/log-out.post.ts
@@ -0,0 +1,13 @@
+// server/api/auth/logout.post.ts
+import { defineEventHandler, setCookie } from 'h3'
+
+export default defineEventHandler((event) => {
+    // Clear the HttpOnly cookie by setting its expiration date in the past or setting its value to empty
+    deleteCookie(event, 'auth_token', {
+        path: '/',
+        httpOnly: true,
+        secure: true, // Ensure the cookie is only sent over HTTPS
+    });
+
+    return { success: true, message: 'Cierre de sesión correcto' };
+});
