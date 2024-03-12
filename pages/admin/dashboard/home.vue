1ea350518e3cac004b7c9fecc29f5d75b3d359ee
Catalina Schencke <catalina.schencke.p@mail.pucv.cl>
2024-03-11 23:17:10 -0300

    remove filter from admin dashboard

--- pages/admin/dashboard/home.vue
+++ pages/admin/dashboard/home.vue
@@ -73,14 +73,7 @@
             <div name="content">
                 <div class="flex items-center justify-between mb-4">
                     <h3 class="text-xl font-medium ">Sesiones futuras</h3>
-                    <button @click="toggleFilterSidebar"
-                        class="bg-secondary text-white px-5 py-2 inline-flex items-center gap-1 rounded-lg">
-                        <span class="sr-only">Open filter sidebar</span>
-                        <Icon class="text-2xl" name="ic:round-filter-list" />
-                        <span>
-                            Filtros
-                        </span>
-                    </button>
+
                 </div>
                 <div class="overflow-x-auto shadow-md sm:rounded-lg">
                     <table class="bg-white w-full table-auto text-sm text-left text-gray-500">
@@ -144,59 +137,9 @@
                     </table>
                 </div>
             </div>
-            <div class="absolute top-0 left-0 w-full h-full min-h-[calc(100vh_-_4.5rem)] bg-black/10 backdrop-blur-[3px] transition-all"
-                :class="{ 'hidden': !filterSidebarOpen }">
-            </div>
+
         </div>
-        <aside id="logo-sidebar"
-            class="fixed top-0 right-0 z-40 w-80 h-screen pt-20 transition-transform bg-white border-l border-gray-200"
-            :class="filterSidebarOpen ? 'translate-x-0' : 'translate-x-full'" aria-label="Sidebar">
-            <div class="h-full px-3 pb-4 overflow-y-auto bg-white">
-                <div class="flex justify-end items-center text-secondary ">
-                    <button @click="filterSidebarOpen = false">
-                        <span>Cerrar</span>
-                        <Icon class="text-4xl" name="heroicons:x-mark-20-solid" />
-                    </button>
-                </div>
-                <form @submit.prevent="filter" class="mx-auto mt-5">
-                    <div class="flex flex-col justify-center gap-y-2">
-                        <select id="selectProfesional" class="text-center py-2 rounded-md bg-[#ededed]">
-                            <option selected>Seleccionar Profesional</option>
-                            <option>Entrenador Físico</option>
-                            <option>Nutricionista</option>
-                        </select>
-                        <select id="selectModality" class="text-center py-2 rounded-md bg-[#ededed]">
-                            <option selected>Seleccionar Modalidad</option>
-                            <option>Online</option>
-                            <option>Presencial</option>
-                        </select>
-                        <select id="selectFormat" class="text-center py-2 rounded-md bg-[#ededed]">
-                            <option selected>Seleccionar Formato</option>
-                            <option>Individual</option>
-                            <option>Grupal</option>
-                        </select>
-                        <div>
-                            <div class="text-center flex justify-center gap-2 py-2 px-5 rounded-md bg-[#ededed]">
-                                <span class="input-group-text" id="basic-addon2">Desde</span>
-                                <input type="date" class="bg-[#ededed] " aria-describedby="basic-addon2" id="desde">
-                            </div>
-                        </div>
-                        <div>
-                            <div class="flex justify-center gap-2 py-2 px-5 rounded-md bg-[#ededed]">
-                                <span class="input-group-text" id="basic-addon2">Hasta</span>
-                                <input type="date" class="bg-[#ededed]" aria-describedby="basic-addon2" id="hasta">
-                            </div>
-                        </div>
-                        <div>
-                            <button type="submit" class="w-full px-5 py-2 rounded-md bg-secondary text-white">
-                                <Icon class="text-2xl" name="ic:round-filter-list" />
-                                Filtrar
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </aside>
+
         <AdminDashboardHomeModifySessionModal :loading="sessionInfoLoading" :sessionInfo="sessionInfo"
             ref="modifySessionModal" />
     </div>
