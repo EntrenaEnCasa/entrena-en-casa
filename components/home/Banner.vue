494b2b0d048504305bcfe2bf9caecba85c651cb6
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-03-19 15:37:13 -0300

    replace router link with NuxtLink and remove styles

--- components/home/Banner.vue
+++ components/home/Banner.vue
@@ -6,7 +6,7 @@
 
                 <div
                     class="col-span-2 grid items-center justify-center w-4/5 lg:w-2/3 text-center lg:text-left lg:ml-10  mb-10 lg:mt-10">
-                    <h1 class="text-4xl   text-black mb-4 tellural font-bold w-full mx-auto lg:mx-0 lg:w-4/5">Personal
+                    <h1 class="text-4xl text-black mb-4 tellural font-bold w-full mx-auto lg:mx-0 lg:w-4/5">Personal
                         Trainers
                         Online,
                         Domicilio y al aire libre
@@ -17,23 +17,23 @@
                         ¿Listo para un cambio?</p>
 
                     <div>
-                        <div class="flex items-center justify-center lg:justify-start mx-auto mt-6 space-x-4 ">
-                            <router-link to="/user/auth/register"
+                        <div class="flex items-center justify-center lg:justify-start mx-auto mt-6 space-x-4">
+                            <NuxtLink to="/user/auth/register"
                                 class="border-0 rounded-full text-white px-6 py-3 font-medium bg-secondary">Registrarse
-                            </router-link>
-                            <nuxtLink to="https://youtu.be/LMNJWdFQrHg" target="_blank"
+                            </NuxtLink>
+                            <NuxtLink to="https://youtu.be/LMNJWdFQrHg" target="_blank"
                                 class="flex items-center space-x-2">
                                 <div
                                     class="rounded-full shadow-lg text-secondary h-12 w-12 font-medium bg-white flex justify-center items-center">
                                     <Icon name="fa6-solid:play" class="ml-1 text-xl" />
                                 </div>
                                 <span class="text-secondary">Ver video</span>
-                            </nuxtLink>
+                            </NuxtLink>
                         </div>
 
                     </div>
                 </div>
-                <div class="hidden lg:block -z-10">
+                <div class="hidden lg:block">
                     <NuxtImg src="/home/banner-img-min.png" alt="Banner"
                         class="absolute h-screen -top-24 right-0 lg:right-16 w-auto object-cover" />
                     <div
@@ -52,9 +52,8 @@
             </div>
         </div>
         <div
-            class="mt-20 lg:mt-12 mx-auto rounded-xl shadow-lg bg-white w-10/12 px-4 py-6 lg:absolute  lg:bottom-0 lg:left-1/2 lg:-translate-x-1/2 lg:translate-y-1/2 ">
-
-            <div class=" grid grid-cols-1 lg:grid-cols-4 gap-4">
+            class="mt-20 lg:mt-12 mx-auto rounded-xl shadow-lg bg-white w-10/12 px-4 py-6 lg:absolute lg:bottom-0 lg:left-1/2 lg:-translate-x-1/2 lg:translate-y-1/2">
+            <div class="grid grid-cols-1 lg:grid-cols-4 gap-4">
                 <div class="w-full flex items-center gap-2">
                     <div>
                         <Icon name="fa6-solid:location-dot" class="text-3xl w-16 min-w-16" />
@@ -91,19 +90,12 @@
                     </div>
                 </div>
                 <div class="flex items-center justify-center mx-auto ">
-                    <router-link to="/user/auth/register" class="border-0 rounded-lg text-white px-3 py-2 font-medium"
-                        style="background-color: #00A4E8">Registrarse
-                        ahora
-                    </router-link>
+                    <NuxtLink to="/user/auth/register"
+                        class="border-0 rounded-lg text-white px-3 py-2 font-medium bg-secondary cursor-pointer">
+                        Registrarse ahora
+                    </NuxtLink>
                 </div>
             </div>
         </div>
     </div>
 </template>
-<style scoped>
-.bg-gradiente {
-    background: url('/background.svg') no-repeat center center fixed;
-    z-index: -20;
-    /* background: linear-gradient(136deg, rgba(198, 243, 255, 0.15) 0%, rgba(113, 212, 240, 0.15) 64.54%, rgba(0, 142, 181, 0.15) 100%); */
-}
-</style>
