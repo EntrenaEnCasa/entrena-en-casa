13530e44a0d61558b3dae6ba41e0f3063ed18aa3
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-03-23 11:04:03 -0300

    if user is logged in, don't allow going to login/register forms

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -11,16 +11,28 @@ export default defineNuxtRouteMiddleware((to, from) => {
     const user = userStore.user;
 
     if (authStore.loggedIn) {
-
-        if (user?.user_type == 2 && !to.path.startsWith("/user")) {
-            return navigateTo('/user/dashboard/home');
+        if (to.path.includes("/auth/login" || "/auth/register")) {
+            // If user is trying to access login page, redirect them to dashboard
+            if (user?.user_type == 2) {
+                return navigateTo('/user/dashboard/home');
+            }
+            else if (user?.user_type == 1) {
+                return navigateTo('/professional/dashboard/home');
+            }
+            else if (user?.user_type == 3) {
+                return navigateTo('/admin/dashboard/home');
+            }
         }
-        else if (user?.user_type == 1 && !to.path.startsWith("/professional")) {
-            return navigateTo('/professional/dashboard/home');
-        }
-        else if (user?.user_type == 3 && !to.path.startsWith("/admin")) {
-            return navigateTo('/admin/dashboard/home');
+        else {
+            if (user?.user_type == 2 && !to.path.startsWith("/user")) {
+                return navigateTo('/user/dashboard/home');
+            }
+            else if (user?.user_type == 1 && !to.path.startsWith("/professional")) {
+                return navigateTo('/professional/dashboard/home');
+            }
+            else if (user?.user_type == 3 && !to.path.startsWith("/admin")) {
+                return navigateTo('/admin/dashboard/home');
+            }
         }
     }
-
 })
