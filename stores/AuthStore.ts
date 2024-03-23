36f3b64af9ed87e34437392b13361a6fd6e322a5
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-03-22 21:41:49 -0300

    add new typescript composition API authstore with proper auth state check
    This checks correctly if the user is authenticated by checking the newly added is_authenticated cookie

---
+++ stores/AuthStore.ts
@@ -0,0 +1,45 @@
+import { useUserStore } from './UserStore'
+import { useToast } from 'vue-toastification'
+
+export const useAuthStore = defineStore( 'AuthStore', () => {
+
+    const loggedIn = ref<Boolean>(false);
+    const userStore = useUserStore();
+    const toast = useToast();
+
+    const logIn = (userData: any) => {
+        loggedIn.value = true;
+        userStore.setUser(userData);
+    }
+
+    const logOut = () => {
+        loggedIn.value = false;
+        userStore.removeUserData();
+        reloadNuxtApp({ path: '/' });
+    }
+
+    const checkAuthState = () => {
+        const isAuthenticated = !!useCookie('is_authenticated').value;
+        if(loggedIn.value && !isAuthenticated) {
+            toast.error('Su sesión ha expirado, por favor ingrese nuevamente');
+        }
+        loggedIn.value = isAuthenticated;
+        if (isAuthenticated) {
+            userStore.setUserFromLocalStorage();
+        }
+    }
+
+    const signUp = (userData: any) => {
+        loggedIn.value = true;
+        userStore.setUser(userData);
+    }
+  
+    return {
+        loggedIn,
+        logIn,
+        logOut,
+        checkAuthState,
+        signUp
+    }
+  
+  });
