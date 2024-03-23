cdab01b545362758896928b89dba840e1b9ddad4
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-03-23 20:59:00 -0300

    correctly fetch data in useGeocoding composable

--- composables/maps/useGeocoding.js
+++ composables/maps/useGeocoding.js
@@ -4,8 +4,14 @@ export const useGeocoding = () => {
     const getReverseGeocodingData = async (coordinates) => {
         const lng = coordinates[0];
         const lat = coordinates[1];
-        const { data } = await useFetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${runtimeConfig.public.mapboxApiKey}`);
-        return data.value.features[0];
+        try {
+            const response = await $fetch(`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${runtimeConfig.public.mapboxApiKey}`);
+            return response.features[0];
+        }
+        catch (error) {
+            console.error(error);
+            return null;
+        }
     };
 
     return { getReverseGeocodingData };
