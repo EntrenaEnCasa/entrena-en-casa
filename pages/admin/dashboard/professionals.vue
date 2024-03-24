522b5c8b93f8d9cbc62a79db5089d7d54f6c16b2
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-03-23 21:00:40 -0300

    remove search bars in some admin pages

--- pages/admin/dashboard/professionals.vue
+++ pages/admin/dashboard/professionals.vue
@@ -2,19 +2,8 @@
     <div class="p-6 sm:p-8">
         <div class="relative">
             <div name="content">
-                <div class="flex items-center justify-between mb-4">
+                <div class="mb-4">
                     <h3 class="text-xl font-medium">Profesionales</h3>
-                    <form novalidate>
-                        <label for="search" class="mb-2 text-sm font-medium text-gray-900 sr-only">Search</label>
-                        <div class="relative">
-                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                                <icon class="text-md text-slate-400" name="fa6-solid:magnifying-glass"></icon>
-                            </div>
-                            <input type="search" id="search"
-                                class="w-full p-2 pl-10 text-sm border border-xl rounded-2xl border-slate-400 placeholder:text-slate-400 focus:ring-secondary-500 focus:border-secondary-500"
-                                placeholder="Buscar..." required />
-                        </div>
-                    </form>
                 </div>
                 <CommonLoading v-if="professionalsDataPending" />
                 <div class="overflow-x-auto shadow-md sm:rounded-lg">
