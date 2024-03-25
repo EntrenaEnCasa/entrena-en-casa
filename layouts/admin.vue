628ecfb279fa1503deb4d3afdc2ccd084a89dd37
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-03-24 21:23:27 -0300

    remove flex and divs from admin and professional layouts

--- layouts/admin.vue
+++ layouts/admin.vue
@@ -4,10 +4,8 @@
         <AdminSidebar ref="sidebar" />
         <main @click="sidebar.sidebarOpen = false" class="mt-[4.5rem] lg:ml-64 transition-[background]">
             <div class="relative">
-                <div class="min-h-[calc(100vh_-_4.5rem)] bg-[#F7FAFA] flex">
-                    <div class="flex-1">
-                        <slot></slot>
-                    </div>
+                <div class="min-h-[calc(100vh_-_4.5rem)] bg-[#F7FAFA]">
+                    <slot></slot>
                 </div>
                 <div class="absolute top-0 w-full h-full min-h-[calc(100vh_-_4.5rem)] bg-black/10 backdrop-blur-[3px] lg:backdrop-blur-none lg:bg-transparent transition-all"
                     :class="{ 'hidden': !sidebar?.sidebarOpen }">
