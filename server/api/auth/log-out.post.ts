62fb84b30e178e322278bd63906efb36889b46a6
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-03-29 22:37:32 -0300

    add domain to deleteCookie method

--- server/api/auth/log-out.post.ts
+++ server/api/auth/log-out.post.ts
@@ -1,17 +1,24 @@
 // server/api/auth/logout.post.ts
 import { defineEventHandler } from 'h3'
 
+
 export default defineEventHandler((event) => {
+
+    const config = useRuntimeConfig(event);
+    const domain = config.public.nodeEnv === 'production' ? '.entrenaencasa.cl' : 'localhost';
+
     // Clear the HttpOnly cookie by setting its expiration date in the past or setting its value to empty
     deleteCookie(event, 'auth_token', {
         path: '/',
         httpOnly: true,
+        domain: domain,
         secure: true, // Ensure the cookie is only sent over HTTPS
     });
 
     // Clear the client-accessible cookie by setting its expiration date in the past or setting its value to empty
     deleteCookie(event, 'is_authenticated', {
         path: '/',
+        domain: domain,
         secure: true,
     });
 
