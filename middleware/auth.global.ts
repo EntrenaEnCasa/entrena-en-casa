d579e5412c230c4f9ec2d792759c5dad466be8be
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-04-03 20:59:48 -0300

    improve global middleware to cover most edge cases

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -11,27 +11,41 @@ export default defineNuxtRouteMiddleware((to, from) => {
     const user = userStore.user;
 
     if (authStore.loggedIn) {
-        if (to.path.includes("/auth/login" || "/auth/register")) {
-            // If user is trying to access login page, redirect them to dashboard
-            if (user?.user_type == 2) {
+        // Si el usuario está logeado, pero no ha verificado su email, y no está en la ruta para verificar el email, será redirigido a la ruta para verificar el email.
+        // El usuario no podrá acceder a ninguna otra ruta hasta que haya verificado su email, a excepción del "aboutYou", que es necesario para que el usuario llene sus datos al momento de registrase.
+        if (user?.user_type == 2 && !user.verified && !to.path.startsWith("/email") && !to.path.includes("/user/dashboard/aboutYou")){
+            return navigateTo('/email/verify-email');
+        }
+        if (!to.path.includes("/dashboard")) {
+            if(user?.user_type == 2){
                 return navigateTo('/user/dashboard/home');
             }
             else if (user?.user_type == 1) {
                 return navigateTo('/professional/dashboard/home');
             }
-            else if (user?.user_type == 3) {
+            else if (user?.user_type == 0) {
                 return navigateTo('/admin/dashboard/home');
             }
         }
-        else {
-            if (user?.user_type == 2 && !to.path.startsWith("/user")) {
-                return navigateTo('/user/dashboard/home');
+    }
+    else{
+
+        // Cuando el usuario no está logeado, si el usuario intenta acceder a la ruta para verificar email, automáticamente lo redirige al login, ya que no tiene sentido que un usuario no logeado pueda verificar su email, ya que para realizar la validación el usuario debe estar logeado para poder obtener los datos necesarios.
+        if (to.path.includes("/email/verify-email")){
+            return navigateTo('/user/auth/login');
+        }
+        else if(to.path.includes("/dashboard")){
+            if(to.path.startsWith("/user")){
+                return navigateTo('/user/auth/login');
             }
-            else if (user?.user_type == 1 && !to.path.startsWith("/professional")) {
-                return navigateTo('/professional/dashboard/home');
+            else if(to.path.startsWith("/professional")){
+                return navigateTo('/professional/auth/login');
             }
-            else if (user?.user_type == 3 && !to.path.startsWith("/admin")) {
-                return navigateTo('/admin/dashboard/home');
+            else if(to.path.startsWith("/admin")){
+                return navigateTo('/admin/auth/login');
+            }
+            else {
+                return navigateTo('/');
             }
         }
     }
