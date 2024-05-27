f0a827f0e1940226b695430c0e2f037a92e2762b
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-05-27 11:44:15 -0400

    simplify rendering logic on EventGrid component

--- components/professional/dashboard/calendar/EventGrid.vue
+++ components/professional/dashboard/calendar/EventGrid.vue
@@ -1,23 +1,23 @@
-<!-- EventGrid component -->
+<!-- EventGrid.vue -->
 <template>
     <div class="overflow-x-auto bg-white rounded-2xl border py-7 px-9">
         <CommonLoading v-if="fetchingEvents" class="my-8" />
         <div v-else>
             <div class="grid" style="grid-template-columns: minmax(60px, max-content) repeat(7, minmax(130px, 1fr));">
                 <div class="text-sm font-semibold text-center border-gray-200"></div>
-                <div v-for="(day, index) in calendarData" :key="index"
+                <div v-for="day in calendarData" :key="day.date"
                     class="capitalize text-sm font-semibold text-center border-b border-gray-200 pb-4 min-w-[130px] text-gray-500">
                     {{ day.formattedDate }}
                 </div>
                 <template v-if="calendarData.length > 0">
-                    <template v-for="(timeSlot, index) in calendarData[0].timeSlots" :key="timeSlot.time">
+                    <template v-for="(timeSlot, index) in timeSlots" :key="timeSlot.time">
                         <div class="text-sm font-semibold text-center border-r border-gray-200 py-2 pr-4 text-gray-500">
                             {{ timeSlot.time }}
                         </div>
-                        <div v-for="(day, dayIndex) in calendarData" :key="`day-${dayIndex}-slot-${index}`">
+                        <div v-for="day in calendarData" :key="day.date">
                             <ProfessionalDashboardCalendarTimeSlot :timeSlot="day.timeSlots[index]" :editMode="editMode"
                                 @emptySlotClick="emptySlotModal.handleClick(day.date, timeSlot.time)"
-                                @eventClick="editEventHandler.handleClick(day.date, timeSlot.time, day.timeSlots[index].event)" />
+                                @eventClick="editEventHandler.handleClick(day.date, timeSlot.time, day.timeSlots[index].events)" />
                         </div>
                     </template>
                 </template>
@@ -39,4 +39,11 @@ const props = defineProps({
     editEventHandler: Object,
 });
 
+// Extract unique time slots from the first day's timeSlots
+const timeSlots = computed(() => {
+    if (props.calendarData.length > 0) {
+        return props.calendarData[0].timeSlots;
+    }
+    return [];
+});
 </script>
