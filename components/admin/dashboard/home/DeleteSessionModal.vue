86b815ff1657a213ef832009c01a8164dc4c9408
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-08-16 12:54:12 -0400

    formatting

--- components/admin/dashboard/home/DeleteSessionModal.vue
+++ components/admin/dashboard/home/DeleteSessionModal.vue
@@ -4,51 +4,82 @@ type: Object,
         <Teleport to="body">
             <CommonModal ref="modal">
                 <div class="w-full p-5 text-center">
-                    <h1 class="text-2xl font-bold">¿De verdad quieres eliminar esta sesión?</h1>
-                    <p class="text-gray-500 mt-2">Esta acción no puede ser revertida</p>
+                    <h1 class="text-2xl font-bold">
+                        ¿De verdad quieres eliminar esta sesión?
+                    </h1>
+                    <p class="text-gray-500 mt-2">
+                        Esta acción no puede ser revertida
+                    </p>
                 </div>
-                <table class="bg-white table-auto text-sm text-center mx-auto mb-2" v-if="sessionInfo">
+                <table
+                    class="bg-white table-auto text-sm text-center mx-auto mb-2"
+                    v-if="sessionInfo">
                     <thead class="text-gray-400">
                         <tr>
-                            <th scope="col" class="font-medium">
-
-                            </th>
-                            <th scope="col" class="font-medium">
-
-                            </th>
+                            <th scope="col" class="font-medium"></th>
+                            <th scope="col" class="font-medium"></th>
                         </tr>
                     </thead>
                     <tbody>
                         <tr>
                             <td class="px-6 py-2">
-                                <div class="grid grid-cols-2 w-5/6 mx-auto gap-5 mb-5 ">
-                                    <p class="font-light text-right  ">Profesional</p>
-                                    <p class=" font-bold text-left">{{ sessionInfo.professional.first_name }} {{
-                    sessionInfo.professional.last_name }} - {{ sessionInfo.professional.title }}</p>
+                                <div
+                                    class="grid grid-cols-2 w-5/6 mx-auto gap-5 mb-5">
+                                    <p class="font-light text-right">
+                                        Profesional
+                                    </p>
+                                    <p class="font-bold text-left">
+                                        {{
+                                            sessionInfo.professional.first_name
+                                        }}
+                                        {{
+                                            sessionInfo.professional.last_name
+                                        }}
+                                        - {{ sessionInfo.professional.title }}
+                                    </p>
                                 </div>
-                                <div class="grid grid-cols-2 w-5/6 mx-auto gap-5 mb-5 ">
-                                    <p class="font-light text-right  ">Fecha</p>
-                                    <p class=" font-bold text-left">{{ sessionInfo.date }}</p>
+                                <div
+                                    class="grid grid-cols-2 w-5/6 mx-auto gap-5 mb-5">
+                                    <p class="font-light text-right">Fecha</p>
+                                    <p class="font-bold text-left">
+                                        {{ sessionInfo.date }}
+                                    </p>
                                 </div>
-                                <div class="grid grid-cols-2 w-5/6 mx-auto gap-5 mb-5 ">
-                                    <p class="font-light text-right  ">Hora</p>
-                                    <p class=" font-bold text-left">{{ sessionInfo.time }} hrs</p>
+                                <div
+                                    class="grid grid-cols-2 w-5/6 mx-auto gap-5 mb-5">
+                                    <p class="font-light text-right">Hora</p>
+                                    <p class="font-bold text-left">
+                                        {{ sessionInfo.time }} hrs
+                                    </p>
                                 </div>
-                                <div class="grid grid-cols-2 w-5/6 mx-auto gap-5 mb-5 ">
-                                    <p class="font-light text-right  ">Modalidad</p>
-                                    <p class=" font-bold text-left">{{ sessionInfo.modality }}</p>
+                                <div
+                                    class="grid grid-cols-2 w-5/6 mx-auto gap-5 mb-5">
+                                    <p class="font-light text-right">
+                                        Modalidad
+                                    </p>
+                                    <p class="font-bold text-left">
+                                        {{ sessionInfo.modality }}
+                                    </p>
                                 </div>
-                                <div class="grid grid-cols-2 w-5/6 mx-auto gap-5 mb-5 ">
-                                    <p class="font-light text-right  ">Formato</p>
-                                    <p class=" font-bold text-left">{{ sessionInfo.format }}</p>
+                                <div
+                                    class="grid grid-cols-2 w-5/6 mx-auto gap-5 mb-5">
+                                    <p class="font-light text-right">Formato</p>
+                                    <p class="font-bold text-left">
+                                        {{ sessionInfo.format }}
+                                    </p>
                                 </div>
-                                <div class="grid grid-cols-2 w-5/6 mx-auto gap-5 mb-5 "
+                                <div
+                                    class="grid grid-cols-2 w-5/6 mx-auto gap-5 mb-5"
                                     v-if="sessionInfo.students.length > 0">
-                                    <p class="font-light text-right  ">Estudiante(s)</p>
+                                    <p class="font-light text-right">
+                                        Estudiante(s)
+                                    </p>
                                     <div>
-
-                                        <p v-for="student in sessionInfo.students" class=" font-bold text-left">{{
-                    student.email }}</p>
+                                        <p
+                                            v-for="student in sessionInfo.students"
+                                            class="font-bold text-left">
+                                            {{ student.email }}
+                                        </p>
                                     </div>
                                 </div>
                             </td>
@@ -56,12 +87,17 @@ type: Object,
                     </tbody>
                 </table>
 
-
                 <div class="flex justify-center items-center p-5">
-                    <CommonButton @click="deleteSession" bg-color="tertiary" class="px-4 py-2 rounded-md mr-2">
+                    <CommonButton
+                        @click="deleteSession"
+                        bg-color="tertiary"
+                        class="px-4 py-2 rounded-md mr-2">
                         Eliminar sesión
                     </CommonButton>
-                    <CommonButton @click="closeModal" bg-color="gray-200" text-color="gray-700"
+                    <CommonButton
+                        @click="closeModal"
+                        bg-color="gray-200"
+                        text-color="gray-700"
                         class="px-4 py-2 rounded-md">
                         Cancelar
                     </CommonButton>
@@ -71,7 +107,7 @@ type: Object,
     </div>
 </template>
 <script setup lang="ts">
-import { useToast } from 'vue-toastification';
+import { useToast } from "vue-toastification";
 const runtimeConfig = useRuntimeConfig();
 
 const toast = useToast();
@@ -88,7 +124,7 @@ interface SessionInfo {
     available: number;
     format: string;
     modality: string;
-    link: string
+    link: string;
     actual_assistant: number;
     type: string;
     coordinates: string | null;
@@ -98,41 +134,38 @@ interface SessionInfo {
 
 const openModal = () => {
     modal.value?.openModal();
-}
+};
 
 const closeModal = () => {
     modal.value?.closeModal();
-}
+};
 
 defineExpose({
     openModal,
-    closeModal
-})
+    closeModal,
+});
 
 const deleteSession = async () => {
     if (!props.sessionInfo) return;
     try {
-        const response = await $fetch<APIResponse>(`${runtimeConfig.public.apiBase}/admin/session/${props.sessionInfo.session_id}`, {
-            method: 'DELETE',
-            credentials: 'include',
-
-        });
+        const response = await $fetch<APIResponse>(
+            `${runtimeConfig.public.apiBase}/admin/session/${props.sessionInfo.session_id}`,
+            {
+                method: "DELETE",
+                credentials: "include",
+            }
+        );
 
         if (response.success) {
             toast.success(response.message);
             reloadNuxtApp();
-        }
-        else {
+        } else {
             toast.error(response.message);
         }
-
-    }
-    catch (error) {
+    } catch (error) {
         console.error(error);
-    }
-    finally {
+    } finally {
         closeModal();
     }
-
-}
+};
 </script>
