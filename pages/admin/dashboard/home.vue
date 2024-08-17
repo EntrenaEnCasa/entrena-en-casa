4377004551e67ff731dddda9d1a77440601cf3ed
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-08-16 22:10:15 -0400

    improve admin home component

--- pages/admin/dashboard/home.vue
+++ pages/admin/dashboard/home.vue
@@ -3,64 +3,62 @@
         <div class="relative">
             <div name="content">
                 <div class="flex items-center justify-between mb-4">
-                    <h3 class="text-xl font-medium ">Sesiones futuras</h3>
+                    <h3 class="text-xl font-medium">Sesiones futuras</h3>
                 </div>
                 <CommonLoading v-if="futureSessionsLoading" />
                 <div v-else class="overflow-x-auto shadow-md sm:rounded-lg">
-                    <table class="bg-white w-full table-auto text-sm text-left text-gray-500">
-                        <thead class="text-xs text-gray-700 uppercase bg-gray-200">
+                    <table
+                        class="bg-white w-full table-auto text-sm text-left text-gray-500">
+                        <thead
+                            class="text-xs text-gray-700 uppercase bg-gray-200">
                             <tr>
-                                <th scope="col" class="p-6">
-                                    Fecha
-                                </th>
-                                <th scope="col" class="p-6">
-                                    Hora
-                                </th>
-                                <th scope="col" class="p-6">
-                                    Modalidad
-                                </th>
-                                <th scope="col" class="p-6">
-                                    Formato
-                                </th>
-                                <th scope="col" class="p-6">
-                                    Profesional
-                                </th>
-                                <th scope="col" class="p-6">
-                                    Alumnos
-                                </th>
-                                <th scope="col" class="p-6">
-                                    Acción
-                                </th>
+                                <th scope="col" class="p-6">Fecha</th>
+                                <th scope="col" class="p-6">Hora</th>
+                                <th scope="col" class="p-6">Modalidad</th>
+                                <th scope="col" class="p-6">Formato</th>
+                                <th scope="col" class="p-6">Profesional</th>
+                                <th scope="col" class="p-6">Alumnos</th>
+                                <th scope="col" class="p-6">Acción</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr v-for="session in futureSessionsData?.sessions" class="border-b"
+                            <tr
+                                v-for="session in futureSessionsData?.sessions"
+                                class="border-b"
                                 :key="session.session_id">
-                                <td class="px-6 py-4 whitespace-nowrap ">
+                                <td class="px-6 py-4 whitespace-nowrap">
                                     {{ session.date }}
                                 </td>
-                                <td class="px-6 py-4 whitespace-nowrap ">
+                                <td class="px-6 py-4 whitespace-nowrap">
                                     {{ session.time }}
                                 </td>
-                                <td class="px-6 py-4 whitespace-nowrap ">
+                                <td class="px-6 py-4 whitespace-nowrap">
                                     {{ session.modality }}
                                 </td>
-                                <td class="px-6 py-4 whitespace-nowrap ">
+                                <td class="px-6 py-4 whitespace-nowrap">
                                     {{ session.format }}
                                 </td>
                                 <td class="px-6 py-4 whitespace-nowrap">
-                                    {{ session.professional.first_name }} {{ session.professional.last_name }}
-                                    <br>
+                                    {{ session.professional.first_name }}
+                                    {{ session.professional.last_name }}
+                                    <br />
                                     {{ session.professional.title }}
                                 </td>
-                                <td class="px-6 py-4 whitespace-nowrap md:whitespace-normal">
-                                    <div class="flex items-center justify-center">
+                                <td
+                                    class="px-6 py-4 whitespace-nowrap md:whitespace-normal">
+                                    <div
+                                        class="flex items-center justify-center">
                                         {{ session.actual_assistant }}
                                     </div>
                                 </td>
                                 <td class="px-6 py-4">
-                                    <button class="px-4 py-2 bg-primary text-white rounded-md font-medium "
-                                        @click="openModalModifySession(session.session_id)">
+                                    <button
+                                        class="px-4 py-2 bg-primary text-white rounded-md font-medium"
+                                        @click="
+                                            openModalModifySession(
+                                                session.session_id
+                                            )
+                                        ">
                                         Ver Detalles
                                     </button>
                                 </td>
@@ -69,21 +67,28 @@
                     </table>
                 </div>
             </div>
-
         </div>
 
-        <AdminDashboardHomeModifySessionModal @refresh-data="test" :loading="sessionInfoLoading"
-            :sessionInfo="sessionInfo" ref="modifySessionModal" />
+        <AdminDashboardHomeModifySessionModal
+            @refresh-data="refreshData"
+            :loading="sessionInfoLoading"
+            :sessionInfo="sessionInfo"
+            ref="modifySessionModal" />
     </div>
 </template>
 
-
 <script lang="ts" setup>
+import { useFormatter } from "@/composables/time/useFormatter";
 
 const runtimeConfig = useRuntimeConfig();
 const sessionInfo = ref<SessionInfo | null>(null);
 const modifySessionModal = ref<Modal | null>(null);
 const sessionInfoLoading = ref<boolean>(false);
+
+onMounted(() => {
+    const date = new Date(2024, 8, 16);
+    console.log(date);
+});
 
 interface SessionInfoResponse extends APIResponse {
     session: SessionInfo;
@@ -100,7 +105,7 @@ interface SessionInfo {
     available: number;
     format: string;
     modality: string;
-    link: string
+    link: string;
     actual_assistant: number;
     type: string;
     coordinates: string | null;
@@ -131,55 +136,54 @@ interface Session {
     actual_assistant: number;
 }
 
-const { data: futureSessionsData, pending: futureSessionsLoading, error, refresh: getFutureSessions } = await useFetch<SessionsResponse>(`${runtimeConfig.public.apiBase}/admin/sessions/future`, {
-    method: 'GET',
-    credentials: 'include',
-    lazy: true
-});
+const {
+    data: futureSessionsData,
+    pending: futureSessionsLoading,
+    error,
+    refresh: getFutureSessions,
+} = await useFetch<SessionsResponse>(
+    `${runtimeConfig.public.apiBase}/admin/sessions/future`,
+    {
+        method: "GET",
+        credentials: "include",
+        lazy: true,
+    }
+);
 
-const test = () => {
-    console.log("test");
+const refreshData = () => {
     getFutureSessions();
-    console.log("test");
-}
-
+};
 
 const filter = () => {
     console.log("filter");
-}
+};
 
 const getSessionInfo = async (session_id: number) => {
     sessionInfoLoading.value = true;
 
     try {
-        const response = await $fetch<SessionInfoResponse>(`${runtimeConfig.public.apiBase}/admin/session/${session_id}`, {
-            method: 'GET',
-            credentials: 'include',
-        });
+        const response = await $fetch<SessionInfoResponse>(
+            `${runtimeConfig.public.apiBase}/admin/session/${session_id}`,
+            {
+                method: "GET",
+                credentials: "include",
+            }
+        );
 
         if (response.success) {
             sessionInfo.value = response.session;
-
-        }
-        else {
+        } else {
             console.error(response.message);
         }
-
-    }
-    catch (error) {
+    } catch (error) {
         console.error(error);
-    }
-    finally {
+    } finally {
         sessionInfoLoading.value = false;
     }
-}
+};
 
 const openModalModifySession = (session_id: number) => {
     getSessionInfo(session_id);
     modifySessionModal.value?.openModal();
-}
-
+};
 </script>
-
-
-<style lang="scss"></style>
