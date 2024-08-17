2f4db3a98668457e92a606cfe371b08f4bb4c8a8
CatalinaSchencke <catalina.schencke.p@mail.pucv.cl>
2024-08-17 17:06:51 -0400

    make CommonSelect make something on update

--- components/common/Select.vue
+++ components/common/Select.vue
@@ -1,18 +1,13 @@
 <template>
     <div>
-        <label v-if="label" class="font-medium text-sm mb-2 inline-block" :for="id">{{
-            label
-        }}</label>
+        <label v-if="label" class="font-medium text-sm mb-2 inline-block" :for="id">
+            {{ label }}
+        </label>
         <div class="flex items-center w-full">
             <div v-if="icon" class="mr-2">
                 <Icon :name="icon" class="text-secondary text-lg" />
             </div>
-            <Field
-                :id="id"
-                :name="name"
-                :modelValue="modelValue"
-                @input="$emit('update:modelValue', $event.target.value)"
-                as="select"
+            <Field :id="id" :name="name" v-model="internalValue" as="select"
                 class="border text-gray-800 text-sm rounded-md w-full px-5 py-3.5 outline-none focus:ring-2 ring-primary"
                 :rules="rules">
                 <option v-if="placeholder" disabled value="">
@@ -29,17 +24,29 @@
 
 <script setup>
 import { Field, ErrorMessage } from "vee-validate";
+import { ref, watch } from 'vue';
 
-defineProps([
-    "modelValue",
-    "placeholder",
-    "name",
-    "label",
-    "type",
-    "icon",
-    "id",
-    "options",
-    "rules",
-]);
-defineEmits(["update:modelValue"]);
+const props = defineProps({
+    modelValue: [String, Number],
+    placeholder: String,
+    name: String,
+    label: String,
+    type: String,
+    icon: String,
+    id: String,
+    options: Array,
+    rules: [String, Object, Array],
+});
+
+const emit = defineEmits(["update:modelValue"]);
+
+const internalValue = ref(props.modelValue);
+
+watch(() => props.modelValue, (newValue) => {
+    internalValue.value = newValue;
+});
+
+watch(internalValue, (newValue) => {
+    emit('update:modelValue', newValue);
+});
 </script>
