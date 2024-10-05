5773889a5d728a861284b54a52885a58b2097603
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-10-05 13:12:21 -0300

    improve footer a bit
    It looked very bad before, now it looks a bit better

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,27 +6,27 @@
             <NuxtLink to="/">
                 <NuxtImg src="/logo-white.png" class="h-full w-40" alt="logo-white" />
             </NuxtLink>
-            <div class="flex flex-wrap gap-4 text-white">
-                <div>
-                    <h5>Contacto</h5>
+            <div class="flex flex-wrap gap-8 text-white">
+                <div class="space-y-4">
+                    <h5 class="text-xl font-bold">Contacto</h5>
                     <div>
                         <p>Correo electrónico</p>
-                        <p>entrenaencasa0@gmail.com</p>
+                        <p class="font-semibold">entrenaencasa0@gmail.com</p>
                     </div>
                     <div>
                         <p>Teléfono</p>
-                        <p>+56 9 7137 0313</p>
+                        <p class="font-semibold">+56 9 7137 0313</p>
                     </div>
                 </div>
-                <div>
-                    <h5>Redes sociales</h5>
+                <div class="space-y-4">
+                    <h5 class="text-xl font-bold">Redes sociales</h5>
                     <div>
                         <p>Facebook</p>
-                        <p>-</p>
+                        <p class="font-semibold">-</p>
                     </div>
                     <div>
                         <p>Instagram</p>
-                        <p>@-</p>
+                        <p class="font-semibold">@-</p>
                     </div>
                 </div>
             </div>
