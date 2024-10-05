addb68c6a4443dd862f7abb58ef91bead6996ee5
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-10-04 22:20:42 -0300

    replace youtube embedded iframe with lite-youtube

--- components/home/WhatIs.vue
+++ components/home/WhatIs.vue
@@ -6,16 +6,12 @@
             <div class="mx-auto grid grid-cols-1 items-center gap-6 lg:grid-cols-3">
                 <div class="lg:col-span-2">
                     <div class="contenedor">
-                        <iframe
+                        <lite-youtube
                             class="responsive-iframe rounded-3xl"
-                            width="560"
-                            height="315"
-                            src="https://www.youtube.com/embed/o_HHBh3WxX0?si=vNy-Qjjoql13hFW4&vq=hd1080&amp;controls=0"
-                            title="YouTube video player"
-                            frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                            allowfullscreen
-                        ></iframe>
+                            videoid="o_HHBh3WxX0"
+                            posterquality="maxresdefault"
+                            params="rel=0&vq=hd1080&controls=0"
+                        ></lite-youtube>
                     </div>
                 </div>
                 <div class="mx-auto w-full font-light leading-tight lg:w-11/12">
@@ -47,3 +43,6 @@
     height: 100%;
 }
 </style>
+<script>
+import "@justinribeiro/lite-youtube";
+</script>
