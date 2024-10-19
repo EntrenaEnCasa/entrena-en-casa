6c0ea47f97771916f1cf4cf6aa5833f1eaac5e33
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-10-19 19:47:39 -0300

    refactor: replace youtube-lite with custom video player component

--- components/home/AboutUs.vue
+++ components/home/AboutUs.vue
@@ -5,13 +5,8 @@
         <div class="mt-10">
             <div class="mx-auto grid grid-cols-1 items-center gap-6 lg:grid-cols-3">
                 <div class="lg:col-span-2">
-                    <div class="contenedor">
-                        <lite-youtube
-                            class="responsive-iframe rounded-3xl"
-                            videoid="o_HHBh3WxX0"
-                            posterquality="maxresdefault"
-                            params="rel=0&vq=hd1080&controls=0"
-                        ></lite-youtube>
+                    <div class="overflow-hidden rounded-3xl">
+                        <CommonVideoPlayer video-src="/videos/entrena-en-casa.mp4" />
                     </div>
                 </div>
                 <div class="mx-auto w-full font-light leading-tight lg:w-11/12">
@@ -25,24 +20,7 @@
         </div>
     </div>
 </template>
-<style scoped>
-.contenedor {
-    position: relative;
-    overflow: hidden;
-    width: 100%;
-    padding-top: 56.25%;
-}
-
-.responsive-iframe {
-    position: absolute;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    width: 100%;
-    height: 100%;
-}
-</style>
+<style scoped></style>
 <script>
 import "@justinribeiro/lite-youtube";
 </script>
