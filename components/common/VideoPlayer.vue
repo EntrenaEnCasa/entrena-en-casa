352e21f89879765b64c0be9db5b98799fbb72739
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-10-19 19:46:24 -0300

    feat: add maximize and compress button for video player

--- components/common/VideoPlayer.vue
+++ components/common/VideoPlayer.vue
@@ -22,6 +22,14 @@
                 <Icon name="fa-solid:play" class="h-8 w-8 text-black" />
             </button>
         </div>
+
+        <!-- Maximize/Compress button -->
+        <button
+            class="absolute bottom-5 right-5 rounded-full bg-white bg-opacity-80 p-3 transition-transform hover:scale-110"
+            @click="toggleFullScreen"
+        >
+            <Icon :name="isFullScreen ? 'fa-solid:compress' : 'fa-solid:expand'" class="h-6 w-6" />
+        </button>
     </div>
 </template>
 
@@ -35,6 +43,7 @@ const props = defineProps({
 
 const videoRef = ref(null);
 const isPlaying = ref(false);
+const isFullScreen = ref(false);
 
 const togglePlay = () => {
     if (videoRef.value.paused) {
@@ -51,5 +60,22 @@ onMounted(() => {
     videoRef.value.addEventListener("ended", () => {
         isPlaying.value = false;
     });
+
+    // Listen to full-screen change events to update the icon
+    document.addEventListener("fullscreenchange", () => {
+        isFullScreen.value = !!document.fullscreenElement;
+    });
 });
+
+// Function to toggle fullscreen mode
+const toggleFullScreen = () => {
+    const videoContainer = videoRef.value.parentElement;
+    if (!document.fullscreenElement) {
+        videoContainer.requestFullscreen().catch((err) => {
+            console.error(`Error attempting to enable full-screen mode: ${err.message}`);
+        });
+    } else {
+        document.exitFullscreen();
+    }
+};
 </script>
