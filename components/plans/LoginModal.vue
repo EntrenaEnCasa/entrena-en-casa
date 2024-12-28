ae4bb9345c20316fe75f9a2ffbbedf8ccad8a25b
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2024-12-28 15:39:52 -0300

    Add new plan login modal component

---
+++ components/plans/LoginModal.vue
@@ -0,0 +1,118 @@
+<template>
+    <CommonModal ref="modalRef">
+        <div class="px-4 py-8">
+            <h2 class="mb-8 text-center text-2xl font-bold">Inicia sesión para continuar</h2>
+            <Form class="mx-auto w-4/5 space-y-5" @submit="login" v-slot="{ meta }">
+                <CommonInput
+                    label="Correo Electrónico"
+                    v-model="formData.email"
+                    name="login-email"
+                    type="email"
+                    id="login-email"
+                    icon="fa6-solid:envelope"
+                    placeholder="Ingresa tu correo electrónico"
+                    :rules="validateEmail"
+                />
+                <CommonInput
+                    label="Contraseña"
+                    v-model="formData.password"
+                    name="login-password"
+                    type="password"
+                    id="login-password"
+                    icon="fa6-solid:lock"
+                    placeholder="* * * * * * * *"
+                    :rules="validatePassword"
+                />
+
+                <div class="flex justify-between">
+                    <router-link to="/password/reset" class="text-secondary">
+                        Olvidé mi contraseña
+                    </router-link>
+                    <button
+                        type="button"
+                        class="text-primary hover:underline"
+                        @click="openRegisterModal"
+                    >
+                        Crear cuenta
+                    </button>
+                </div>
+                <CommonButton
+                    class="w-full py-2 font-medium"
+                    text-size="xl"
+                    :loading="loading"
+                    :disabled="!meta.valid"
+                >
+                    Iniciar Sesión
+                </CommonButton>
+            </Form>
+        </div>
+    </CommonModal>
+</template>
+
+<script setup>
+import { useToast } from "vue-toastification";
+
+const modalRef = ref(null);
+const emit = defineEmits(["openRegister"]);
+const toast = useToast();
+const authStore = useAuthStore();
+const router = useRouter();
+
+const formData = reactive({
+    email: "",
+    password: "",
+});
+
+const loading = ref(false);
+
+const validateEmail = (email) => {
+    if (!email) return "El email es requerido";
+
+    const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
+    if (!regex.test(email)) return "El email no es válido";
+    return true;
+};
+
+const validatePassword = (password) => {
+    if (!password) return "La contraseña es requerida";
+    return true;
+};
+
+const login = async () => {
+    loading.value = true;
+    try {
+        const response = await $fetch("/api/auth/student/log-in", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: {
+                email: formData.email,
+                password: formData.password,
+            },
+        });
+
+        if (response.success) {
+            authStore.logIn(response.user);
+            modalRef.value.closeModal();
+            toast.success("Inicio de sesión exitoso");
+            router.push("/user/dashboard/credits");
+        } else {
+            toast.error(response.message);
+        }
+    } catch (error) {
+        console.error(error);
+        toast.error("Ocurrió un error al iniciar sesión");
+    } finally {
+        loading.value = false;
+    }
+};
+
+const openRegisterModal = () => {
+    modalRef.value.closeModal();
+    emit("openRegister");
+};
+
+defineExpose({
+    openModal: () => modalRef.value?.openModal(),
+    closeModal: () => modalRef.value?.closeModal(),
+});
+</script>
