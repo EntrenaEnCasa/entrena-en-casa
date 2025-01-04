596025f8bb7ce2435075de79ea9ce7e8ac3c8204
Gonzalo Bruna <gonzalo.antoniobc@gmail.com>
2025-01-04 18:57:34 -0300

    Complete refactor of plan-related pages
    This greatly improves all plan-related pages, making them much easier to work with, following the same design patterns and making them easier for the users to understand.
    This greatly reduces the amount of lines of code needed for these pages and also helps separate concerns by moving types to the types directory.
    It also makes use of typescript everywhere making it much easier to see errors and make refactors.

--- pages/plans/[id].vue
+++ pages/plans/[id].vue
@@ -1,11 +1,14 @@
+// pages/plans/[id].vue
 <template>
     <div class="bg-gradiente min-h-[calc(100vh_-_5rem)] py-10">
         <div class="mx-auto w-11/12 max-w-6xl">
             <div v-if="pending">
                 <CommonLoading />
             </div>
-            <div v-else-if="error">Hubo un error al obtener el plan</div>
-            <div v-else class="space-y-8">
+            <div v-else-if="error" class="text-center text-red-500">
+                Hubo un error al obtener el plan
+            </div>
+            <div v-else-if="plan" class="space-y-8">
                 <!-- Back button -->
                 <NuxtLink
                     to="/plans"
@@ -15,118 +18,51 @@
                     <span>Volver a planes</span>
                 </NuxtLink>
 
-                <!-- Plan Details Card -->
-                <div class="rounded-xl bg-white p-8 shadow">
-                    <div class="space-y-6">
-                        <!-- Price and Title Section -->
-                        <div class="text-center">
-                            <h2 class="tellural mb-4 text-4xl font-bold text-secondary">
-                                {{ plan.formattedPrice }}
-                            </h2>
-                            <h3 class="text-2xl font-semibold text-gray-800">
-                                Plan de {{ plan.credit_quantity }} sesiones
-                                {{
-                                    plan.format_credit === "Individual"
-                                        ? "individuales"
-                                        : plan.format_credit === "Dupla"
-                                          ? "dupla"
-                                          : "grupales"
-                                }}
-                            </h3>
-                        </div>
-
-                        <!-- Description and Details -->
-                        <div class="space-y-4">
-                            <p class="text-center text-lg">
-                                {{ plan.description }}
-                            </p>
-                            <p class="text-center font-medium">
-                                Duración del plan: {{ plan.expiration_time }}
-                            </p>
-                        </div>
-
-                        <!-- Features List -->
-                        <ul class="space-y-4">
-                            <li class="flex gap-2">
-                                <div
-                                    class="flex h-6 min-h-6 w-6 min-w-6 items-center justify-center rounded-full bg-secondary-50/40"
-                                >
-                                    <Icon name="mingcute:check-fill" class="text-secondary" />
-                                </div>
-                                <p>Entrenamiento con Personal Trainer el 100% de la sesión</p>
-                            </li>
-                            <li class="flex gap-2">
-                                <div
-                                    class="flex h-6 min-h-6 w-6 min-w-6 items-center justify-center rounded-full bg-secondary-50/40"
-                                >
-                                    <Icon name="mingcute:check-fill" class="text-secondary" />
-                                </div>
-                                <p>Horario a convenir</p>
-                            </li>
-                            <li class="flex gap-2">
-                                <div
-                                    class="flex h-6 min-h-6 w-6 min-w-6 items-center justify-center rounded-full bg-secondary-50/40"
-                                >
-                                    <Icon name="mingcute:check-fill" class="text-secondary" />
-                                </div>
-                                <p>
-                                    {{
-                                        plan.format_credit === "Individual"
-                                            ? "Formato personalizado, un tiempo solo para ti"
-                                            : plan.format_credit === "Dupla"
-                                              ? "Formato en dupla, siempre con alguien de confianza"
-                                              : "Formato grupal, entrena junto a la comunidad de Entrena en Casa"
-                                    }}
-                                </p>
-                            </li>
-                        </ul>
-
-                        <!-- Action Buttons -->
-                        <div class="space-y-3 pt-4">
-                            <CommonButton
-                                @click="handleBuyPlan"
-                                class="w-full bg-secondary px-4 py-2"
-                                rounded-size="full"
-                            >
-                                Comprar plan
-                            </CommonButton>
-                            <CommonButton
-                                @click="handleContactSales"
-                                class="w-full bg-primary px-4 py-2"
-                                rounded-size="full"
-                            >
-                                Hablar con ventas
-                            </CommonButton>
-                        </div>
-                    </div>
-                </div>
+                <!-- Plan Card -->
+                <PlansPlanCard :plan="plan">
+                    <template #primary-action>
+                        <CommonButton
+                            @click="handleBuyPlan"
+                            class="w-full bg-secondary px-4 py-2"
+                            rounded-size="full"
+                        >
+                            Comprar plan
+                        </CommonButton>
+                    </template>
+                </PlansPlanCard>
             </div>
         </div>
+
+        <!-- Authentication Modals -->
         <PlansLoginModal ref="loginModalRef" @openRegister="openRegisterModal" />
         <PlansRegisterModal ref="registerModalRef" @openLogin="openLoginModal" />
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import type { GetPlanResponse } from "@/types/api/plans";
+
 const config = useRuntimeConfig();
 const route = useRoute();
 const authStore = useAuthStore();
 
-const loginModalRef = ref(null);
-const registerModalRef = ref(null);
+// Modal refs
+const loginModalRef = ref<Modal | null>(null);
+const registerModalRef = ref<Modal | null>(null);
 
 // Fetch plan data
 const {
     data: response,
     pending,
     error,
-} = await useFetch(`${config.public.apiBase}/user/plans/${route.params.id}`, {
+} = await useFetch<GetPlanResponse>(`${config.public.apiBase}/user/plans/${route.params.id}`, {
     method: "GET",
 });
 
 // Computed property to get the plan from the response
 const plan = computed(() => (response.value?.success ? response.value.plan : null));
 
+// Action Handlers
 const handleBuyPlan = () => {
     if (!authStore.loggedIn) {
         loginModalRef.value?.openModal();
@@ -134,32 +70,6 @@ const handleBuyPlan = () => {
     }
     // TODO: Handle purchase logic here
     console.log("Plan to purchase: ", plan.value);
-};
-
-const handleContactSales = async () => {
-    if (!plan.value) return;
-    const modality = formatPlan(plan.value.credit_type);
-
-    const link = `https://wa.me/56971370313?text=Hola%20Jorge,%20quiero%20contratar%20un%20plan%20${modality}%20${plan.value.format_credit}%20${plan.value.credit_quantity}%20sesiones%20.`;
-
-    await navigateTo(link, {
-        external: true,
-        open: {
-            target: "_blank",
-        },
-    });
-};
-
-const formatPlan = (creditType) => {
-    if (creditType === "PP") {
-        return "Personalizado Presencial";
-    } else if (creditType === "GP") {
-        return "Grupal Presencial";
-    } else if (creditType === "PO") {
-        return "Personalizado Online";
-    } else if (creditType === "GO") {
-        return "Grupal Online";
-    }
 };
 
 const openLoginModal = () => {
--- pages/plans/index.vue
+++ pages/plans/index.vue
@@ -1,7 +1,10 @@
+// pages/plans/index.vue
 <template>
     <div class="bg-gradiente min-h-[calc(100vh_-_5rem)] py-10">
         <div class="mx-auto w-11/12 max-w-6xl">
             <h2 class="tellural mb-10 text-center text-4xl font-bold">Nuestros planes</h2>
+
+            <!-- Filters Section -->
             <div class="mx-auto mb-10 grid grid-cols-1 gap-10 lg:grid-cols-3">
                 <CommonSelect
                     label="Región"
@@ -25,112 +28,47 @@
                     :options="sessionModalities"
                 />
             </div>
+
+            <!-- Loading and Error States -->
             <div v-if="plansLoading">
                 <CommonLoading />
             </div>
-            <div v-else-if="error">Hubo un error al obtener los planes</div>
-            <div v-else-if="plansResponse.plans.length === 0">
+            <div v-else-if="error" class="text-center text-red-500">
+                Hubo un error al obtener los planes
+            </div>
+            <div v-else-if="!plansResponse?.plans.length" class="text-center text-gray-600">
                 No hay planes disponibles para la región, formato y modalidad seleccionada
             </div>
+
+            <!-- Plans Grid -->
             <div v-else class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
-                <div
-                    v-for="plan in plansResponse.plans"
-                    class="flex flex-col items-center space-y-5 rounded-xl bg-white p-8 shadow"
-                >
-                    <h3 class="text-3xl font-semibold text-secondary">
-                        {{ plan.formattedPrice }}
-                    </h3>
-                    <h4 class="text-center text-2xl font-semibold text-gray-800">
-                        Plan de {{ plan.credit_quantity }} sesiones
-                        {{
-                            format === "Individual"
-                                ? "individuales"
-                                : format === "Dupla"
-                                  ? "dupla"
-                                  : "grupales"
-                        }}
-                    </h4>
-                    <p class="text-center font-medium">
-                        {{ plan.description }}
-                    </p>
-                    <p class="text-center font-medium">
-                        Duración del plan: {{ plan.expiration_time }}
-                    </p>
-                    <ul class="space-y-3">
-                        <li class="flex gap-2">
-                            <div
-                                class="flex h-6 min-h-6 w-6 min-w-6 items-center justify-center rounded-full bg-secondary-50/40"
-                            >
-                                <Icon name="mingcute:check-fill" class="text-secondary" />
-                            </div>
-                            <p>Entrenamiento con Personal Trainer el 100% de la sesión</p>
-                        </li>
-                        <li class="flex gap-2">
-                            <div
-                                class="flex h-6 min-h-6 w-6 min-w-6 items-center justify-center rounded-full bg-secondary-50/40"
-                            >
-                                <Icon name="mingcute:check-fill" class="text-secondary" />
-                            </div>
-                            <p>Horario a convenir</p>
-                        </li>
-                        <li class="flex gap-2">
-                            <div
-                                class="flex h-6 min-h-6 w-6 min-w-6 items-center justify-center rounded-full bg-secondary-50/40"
-                            >
-                                <Icon name="mingcute:check-fill" class="text-secondary" />
-                            </div>
-                            <p>
-                                {{
-                                    format === "Individual"
-                                        ? "Formato personalizado, un tiempo solo para ti"
-                                        : format === "Dupla"
-                                          ? "Formato en dupla, siempre con alguien de confianza"
-                                          : "Formato grupal, entrena junto a la comunidad de Entrena en Casa"
-                                }}
-                            </p>
-                        </li>
-                    </ul>
-                    <div class="w-full space-y-2">
-                        <CommonButton
-                            :to="`/plans/${plan.plan_id}`"
-                            class="w-full bg-secondary px-4 py-2"
-                            rounded-size="full"
-                        >
-                            Ver detalles del plan
-                        </CommonButton>
-                        <CommonButton
-                            @click="
-                                goToWhatsapp(
-                                    plan.format_credit,
-                                    plan.credit_type,
-                                    plan.credit_quantity,
-                                )
-                            "
-                            class="w-full bg-primary px-4 py-2"
-                            rounded-size="full"
-                        >
-                            Hablar con ventas
-                        </CommonButton>
-                    </div>
-                </div>
+                <PlansPlanCard
+                    v-for="plan in plansResponse?.plans"
+                    :key="plan.plan_id"
+                    :plan="plan"
+                />
             </div>
         </div>
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import type { GetPlansResponse } from "@/types/api/plans";
+
 const config = useRuntimeConfig();
 
-const region = ref(13);
-const format = ref("Individual");
-const modality = ref("P");
+// State
+const region = ref<number>(13);
+const format = ref<"Individual" | "Dupla" | "Grupal">("Individual");
+const modality = ref<"P" | "O">("P");
+
+// Computed
 const creditType = computed(() => {
-    return format.value === "Dupla" || format.value === "Individual"
-        ? "P" + modality.value
-        : "G" + modality.value;
+    return (format.value === "Dupla" || format.value === "Individual" ? "P" : "G") + modality.value;
 });
 
-const regionOptions = ref([
+// Options for selects
+const regionOptions = ref<SelectOption[]>([
     { value: 11, label: "Aisén del General Carlos Ibañez del Campo" },
     { value: 2, label: "Antofagasta" },
     { value: 15, label: "Arica y Parinacota" },
@@ -149,65 +87,23 @@ const regionOptions = ref([
     { value: 5, label: "Valparaíso" },
 ]);
 
-const sessionFormats = ref([
+const sessionFormats = ref<SelectOption[]>([
     { value: "Individual", label: "Individual" },
     { value: "Dupla", label: "Dupla" },
     { value: "Grupal", label: "Grupal" },
 ]);
 
-const sessionModalities = ref([
+const sessionModalities = ref<SelectOption[]>([
     { value: "P", label: "Presencial" },
     { value: "O", label: "Online" },
 ]);
 
-const formatPlan = (creditType) => {
-    if (creditType === "PP") {
-        return "Personalizado Presencial";
-    } else if (creditType === "GP") {
-        return "Grupal Presencial";
-    } else if (creditType === "PO") {
-        return "Personalizado Online";
-    } else if (creditType === "GO") {
-        return "Grupal Online";
-    }
-};
-
-const goToWhatsapp = async (planFormat, planType, planCreditQuantity) => {
-    if (!planFormat || !planType || !planCreditQuantity) return;
-    const modality = formatPlan(planType);
-    const planRegion = formatRegion(region.value);
-
-    const link =
-        "https://wa.me/56971370313?text=Hola%20Jorge,%20quiero%20contratar%20un%20plan%20" +
-        modality +
-        "%20" +
-        planFormat +
-        "%20" +
-        planCreditQuantity +
-        "%20sesiones%20en%20la%20región%20de%20" +
-        planRegion +
-        ".";
-    await navigateTo(link, {
-        external: true,
-        open: {
-            target: "_blank",
-        },
-    });
-
-    return;
-};
-
-const formatRegion = (region) => {
-    if (!region) return "";
-    const selectedRegion = regionOptions.value.find((r) => r.value === region);
-    return selectedRegion ? selectedRegion.label : "";
-};
-
+// API call
 const {
     data: plansResponse,
     pending: plansLoading,
     error,
-} = await useFetch(`${config.public.apiBase}/user/plans`, {
+} = await useFetch<GetPlansResponse>(`${config.public.apiBase}/user/plans`, {
     method: "POST",
     body: {
         region: region,
@@ -216,4 +112,6 @@ const {
     },
     watch: [region, creditType, format],
 });
+
+console.log(plansResponse.value);
 </script>
