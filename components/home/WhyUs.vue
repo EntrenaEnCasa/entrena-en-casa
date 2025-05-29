4b5119fca13a5dcc981b47dbc19f00051402b33d
CatalinaSchencke <catalina.schencke.p@mail.pucv.cl>
2025-05-28 23:18:24 -0400

    delete image of woman in why us page

--- components/home/WhyUs.vue
+++ components/home/WhyUs.vue
@@ -1,21 +1,22 @@
 <template>
-    <div class="my-5">
-        <div class="mx-auto w-11/12 text-center">
+    <div class="my-5 grid grid-cols-1 lg:grid-cols-2 lg:p-10">
+        <div class="mx-auto w-3/4 text-center  lg:items-end">
             <h3 class="tracking-[.3rem]">BENEFICIOS</h3>
             <h2 class="my-3 text-4xl font-medium">¿Por qué Entrena en Casa?</h2>
             <p class="font-light">
                 Entrena en Casa es la nueva forma de ejercitarse y vivir de forma más saludable.
             </p>
         </div>
-        <div class="my-10 flex justify-between">
-            <NuxtImg
+        <!-- En lg ocuparia  2 de los 3 espacios el siguiente div -->
+        <div class="my-10  lg:my-0 lg:w-11/12">
+            <!-- <NuxtImg
                 class="hidden xl:block"
                 src="/home/woman-exercising.png"
                 alt="woman exercising"
                 width="500"
-            />
-            <div class="mx-auto flex w-11/12 items-center justify-center">
-                <ul class="flex flex-col items-center space-y-2 text-start">
+            /> -->
+            <div class="mx-auto flex w-11/12 items-center lg:justify-start justify-center">
+                <ul class="flex flex-col items-center lg:items-start space-y-2 text-start">
                     <li class="flex w-3/4 items-center">
                         <Icon name="mingcute:check-fill" class="mr-2 text-4xl text-secondary" />
                         <p class="font-light">
