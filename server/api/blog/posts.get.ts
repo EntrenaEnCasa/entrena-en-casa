753086af98de1982da3b9b2dcaf2be4cd7e9bbbd
CatalinaSchencke <catalina.schencke.p@mail.pucv.cl>
2025-07-23 22:12:16 -0400

    update post.get.ts

--- server/api/blog/posts.get.ts
+++ server/api/blog/posts.get.ts
@@ -1,11 +1,11 @@
 import type { BlogPost, BlogResponse } from '~/types/blog'
 import path from 'path'
+
 export default defineEventHandler(async (event): Promise<BlogResponse> => {
     try {
-        // Obtener parámetros de paginación
         const query = getQuery(event)
         const page = Number(query.page) || 1
-        const limit =  5
+        const limit = 5
 
         const { posts, total } = await readBlogPosts(page, limit)
         return {
@@ -24,21 +24,23 @@ export default defineEventHandler(async (event): Promise<BlogResponse> => {
 
 async function readBlogPosts(page: number, limit: number): Promise<{ posts: BlogPost[], total: number }> {
   try {
-    // Leer desde único archivo blog.json
-    const blogFile = path.join(process.cwd(), 'content', 'blog/blog.json')
+    const blogFile = path.join(process.cwd(), '.output/public', 'blog/blog.json')
     const fs = await import('fs')
+    
     if (!fs.existsSync(blogFile)) {
       console.warn('Blog file does not exist:', blogFile)
       return { posts: [], total: 0 }
     }
+    
     const content = fs.readFileSync(blogFile, 'utf8')
     const posts: BlogPost[] = JSON.parse(content)
-    // Ordenar de más nuevo a más viejo
+    
     const sortedPosts = posts.sort((a, b) => new Date(b.publishedAt).getTime() - new Date(a.publishedAt).getTime())
     const total = sortedPosts.length
     const start = (page - 1) * limit
     const end = start + limit
     const pagePosts = sortedPosts.slice(start, end)
+    
     return { posts: pagePosts, total }
   } catch (error) {
     console.error('Error reading blog posts:', error)
